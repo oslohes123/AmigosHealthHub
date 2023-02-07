a255acae51dc8990fe237b64cfa81ca16ca26052
V1SHAL421 <70663959+V1SHAL421@users.noreply.github.com>
2023-02-07 16:45:36 +0000

    Implement searchDB to query database for exercise

--- Server/controllers/fitnessController.js
+++ Server/controllers/fitnessController.js
@@ -1,16 +1,33 @@
-async function checkDB(){
+async function searchDB(){
     const supabase = require('../dist/utils/supabaseSetUp.js');
     const supabaseQuery = require('../dist/utils/databaseInterface.js');
     const checker = new supabaseQuery()
-    const {data,error} = await checker.select(supabase, "Exercises", {Type:"Cardio", Name:"Run", Muscle:"", Difficulty:"Advanced", Instructions:"Run"}, "Muscle","");
+    var name = 'Incline Hammer Curls';
+    const {data,error} = await checker.select(supabase, "Exercises", "Name");
     if(error) console.error(error);
-    else if (data == ""){
-        //API CALL
-    }
-    else{
+    else if (data.includes(name)){
         console.log({data});
         //Run Exercise
     }
+    else {
+        const req = require('req');
+
+        req.get({
+        url: 'https://api.api-ninjas.com/v1/exercises?name=' + name,
+        headers: {
+    'X-Api-Key': 'YOUR_API_KEY'
+  },
+}, 
+  function(error, response, body) {
+    if(error) return console.error('Request failed:', error);
+    else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+    else console.log(body)
+});
+    const {data,error} = await checker.insert(supabase, "Exercises", {Type:"Weight", Name:name, Muscle:"biceps", Difficulty:"Advanced", Instructions:"Run"});
+    if(error) console.error(error);
+    else console.log({data})
+    }
+    
 }
 function fitnessMainPage(req,res){
     res.send("Fitness Page");
