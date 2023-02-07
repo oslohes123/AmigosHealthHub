73673cf155e223e8c843eca4aa18557a919a3c88
ethanjhutchins <ethanjameshutchins@gmail.com>
2023-02-07 15:43:55 +0000

    Add basic front end with navigation functionality.

---
+++ Client/navigation/screens/Fitness/FitnessDashboardScreen.js
@@ -0,0 +1,61 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, SafeAreaView, Image } from 'react-native';
+
+//Screen Names
+const trackWorkoutName = 'Workout Plans'
+const fitnessDashboardName = 'Fitness Dashboard'
+
+export default function FitnessScreen({ navigation }) {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title} onLongPress={() => {
+                console.log("The user wants to see info about the fitness page.")
+            }}>
+                Fitness Dashboard
+            </Text>
+            <View style={styles.blankSpace}>
+                {/* <Image style={styles.mainImage} source={require('../../assets/favicon.png')} /> */}
+            </View>
+
+            <View style={styles.buttonView}>
+                <Button title="Workouts Plans" onPress={() => {
+                    console.log("The user wants to see their workout plans.")
+                    navigation.navigate("Workout Plans")
+                }} />
+                <Button title="Track Exercises" onPress={() => {
+                    console.log("The user wants to track a single exercise.")
+                }} />
+            </View>
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 32,
+        fontWeight: "bold",
+        padding: 40,
+    },
+    blankSpace: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    mainImage: {
+        width: 120,
+        height: 120,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    buttonView: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        padding: 40,
+    }
+});
---
+++ Client/navigation/screens/MainDashboard/DashboardScreen.js
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import react from "react";
+import { StyleSheet, Text, View, Button, SafeAreaView, Image } from 'react-native';
+
+export default function DashboardScreen({ navigation }) {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title} onLongPress={() => {
+                console.log("The user wants to see info about the dashboard.")
+            }}>
+                DASHBOARD
+            </Text>
+            <View style={styles.blankSpace}>
+                {/* <Image style={styles.mainImage} source={require('assets/favicon.png')} /> */}
+            </View>
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 32,
+        fontWeight: "bold",
+        padding: 40,
+    },
+    blankSpace: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    mainImage: {
+        width: 120,
+        height: 120,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    button: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    }
+});
