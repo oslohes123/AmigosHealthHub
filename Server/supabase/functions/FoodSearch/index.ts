51d1dab1c6201c883a112fa330107e73beec50e2
Ashley Tyagi <ashleytyagi@hotmail.com>
2023-02-09 15:42:22 +0000

    update main function takes in a code and a value which is used to understand which method to call Also provide a curl command that can be used for testing purposes

--- Server/supabase/functions/FoodSearch/index.ts
+++ Server/supabase/functions/FoodSearch/index.ts
@@ -3,77 +3,36 @@
 // This enables autocomplete, go to definition, etc.
 
 import {serve} from "https://deno.land/std@0.168.0/http/server.ts";
-
-const baseUrl = "https://trackapi.nutritionix.com/v2/";
-
-async function instantSearch(food: string): Promise<JSON> {
-    try {
-        return (await fetch(baseUrl + "search/instant?query=" + food, {
-            method: "GET",
-            headers: {
-                "x-app-id": Deno.env.get('X_APP_ID') as string,
-                "x-app-key": Deno.env.get('X_APP_KEY') as string,
-            },
-        })).json();
-
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
-
-
-async function brandedSearch(nix_item_id: string): Promise<JSON> {
-    try {
-        return (await fetch(baseUrl + "search/item?nix_item_id=" + nix_item_id, {
-            method: "GET",
-            headers: {
-                "x-app-id": Deno.env.get('X_APP_ID') as string,
-                "x-app-key": Deno.env.get('X_APP_KEY') as string,
-            },
-        })).json();
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
-
-
-instantSearch("apple").then(
-    output => {
-        console.log(output[0].common)
-    }
-)
-
-
-
-
+import {searchMethods} from "./constants.ts";
+import instantSearch from "./searches/instantFoodSearch.ts";
+import brandedSearch from "./searches/brandedSearch.ts";
+import nutrientSearch from "./searches/nutrientSearch.ts";
 
 serve(async (req) => {
-    const {name} = await req.json();
-    const data = {
-        message: `Hello my name is ${name}!`,
-    };
+    const {value:inputData, code} = await req.json();
+    let data;
+    switch (code) {
+        case searchMethods.instantSearch:
+            data = await instantSearch(inputData)
+            break;
+        case searchMethods.nutrientSearch:
+            data = await nutrientSearch(inputData)
+            break;
+        case searchMethods.brandedSearch:
+            data = await brandedSearch(inputData)
+            break;
+        default:
+            data = `The code was incorrect as ${code}`
+    }
+
 
     return new Response(JSON.stringify(data), {
         headers: {"Content-Type": "application/json"},
     });
 });
 
-// fetch(baseUrl + "search/instant?query=balls", {
-//   method: "GET",
-//   headers: {
-//     "x-app-id": "8aca1c28",
-//     "x-app-key": "f2f9ec07613190d7278f404a8e26dda8",
-//   },
-// })
-//     .then((response) => response.json())
-//     .then((response) => console.log(response))
-//     .catch((err) => console.log(err));
-
-
 // // To invoke:
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 //   --header 'Content-Type: application/json' \
-//   --data '{"name":"Ashley Tyagi"}'
+//   --data '{"value":"banana","code":1}'
