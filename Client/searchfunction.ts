42b6f394f28beaa13f195688eb07c7d275fea3ad
Ashley Tyagi <ashleytyagi@hotmail.com>
2023-02-12 14:26:31 +0000

    Update searchfunction to use interface

--- Client/searchfunction.ts
+++ Client/searchfunction.ts
@@ -1,23 +1,25 @@
 import {createClient} from '@supabase/supabase-js'
 import {anonKey, searchMethods, supabaseURL} from "./constants";
-
+import {searchInterface} from "./searchInterface";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(supabaseURL, anonKey)
 
 
-async function nutrientSearch(input:string) {
+async function nutrientSearch(input_data: searchInterface) {
     const {data, error} = await supabase.functions.invoke("FoodSearch", {
-        body: {value:input, code: searchMethods.nutrientSearch},
+        body: input_data,
     })
-    if(error) {
+    if (error) {
         console.log(error)
         return error;
-    }else{
+    } else {
         return data
     }
 }
 
-let x = nutrientSearch("apple")
+let a: searchInterface = {value: "apples", code: searchMethods.instantSearch}
+
+let x = nutrientSearch(a)
 
 x.then(response => console.log(response))
