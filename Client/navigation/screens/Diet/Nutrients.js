360004a81445dbd82d00733e1abcd988d2b55eb8
shazeen123 <shazeenshaheen2003@gmail.com>
2023-02-16 15:17:06 +0000

    create table for total consumed nutrients

--- Client/navigation/screens/Diet/Nutrients.js
+++ Client/navigation/screens/Diet/Nutrients.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-//import { Table, Row, Rows } from 'react-native-table-component';
+import { Table, Row, Rows } from 'react-native-table-component';
 
-// const header = ['Nutrient', 'Amount'];
-// const data = [
-//   ['Protein', '82g'],
-//   ['Carbohydrate', '65g'],
-//   ['Fat', '22g'],
-//   ['Energy', '46g'],
-//   ['Sugars', '15g'],
-//   ['Fibre', '49g'],
-//   ['Vitamins', '60g'],
-// ];
+const header = ['Nutrient', 'Amount'];
+const data = [
+  ['Protein', '82g'],
+  ['Carbohydrate', '65g'],
+  ['Fat', '22g'],
+  ['Energy', '46g'],
+  ['Sugars', '15g'],
+  ['Fibre', '49g'],
+  ['Vitamins', '60g'],
+];
 
 export default function Nutrients() {
   return (
-    // <View style={styles.container}>
-    //   <View style={styles.tableContainer}>
-    //     <Text style={styles.header}>Food Consumed</Text>
-    //     <Table borderStyle={styles.tableBorder}>
-    //       <Row
-    //         data={header}
-    //         style={styles.tableHeader}
-    //         textStyle={styles.tableHeaderText}
-    //       />
-    //       <Rows data={data} textStyle={styles.tableRowText} />
-    //     </Table>
-    //   </View>
-    // </View>
     <View style={styles.container}>
-      <Text style={styles.header}>Food Consumed</Text>
+      <View style={styles.tableContainer}>
+        <Text style={styles.header}>Food Consumed</Text>
+        <Table borderStyle={styles.tableBorder}>
+          <Row
+            data={header}
+            style={styles.tableHeader}
+            textStyle={styles.tableHeaderText}
+          />
+          <Rows data={data} textStyle={styles.tableRowText} />
+        </Table>
+      </View>
     </View>
+    // <View style={styles.container}>
+    //   <Text style={styles.header}>Food Consumed</Text>
+    // </View>
   );
 }
 
@@ -39,42 +39,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#0C1E3F',
     flex: 1,
   },
-  // tableContainer: {
-  //   marginTop: 50,
-  //   alignSelf: 'center',
-  //   width: 350
-  // },
+  tableContainer: {
+    marginTop: 40,
+    alignSelf: 'center',
+    width: 350
+  },
   header: {
     //width: 300,
     fontSize: 30,
     fontWeight: 'bold',
-    marginTop: 40,
+    marginBottom: 15,
     color: 'white',
     alignSelf: 'center'
   },
-  // tableBorder: {
-  //   borderWidth: 2,
-  //   borderColor: 'white',
-  // },
-  // tableHeader: {
-  //   //backgroundColor: '#f2f2f2',
-  //   height: 70,
-  //   //borderBottomWidth: 2,
-  //   //borderBottomColor: '#fff',
-  // },
-  // tableHeaderText: {
-  //   fontSize: 25,
-  //   fontWeight: 'bold',
-  //   color: 'white',
-  //   alignSelf: 'center'
-  // },
-  // tableRowText: {
-  //   fontSize: 16,
-  //   color: 'white',
-  //   alignSelf: 'center',
-  //   padding: 15,
-  //   height: 70,
-  //   marginTop: 10,
-  //   marginBottom: -10
-  // },
+  tableBorder: {
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  tableHeader: {
+    //backgroundColor: '#f2f2f2',
+    height: 70,
+    //borderBottomWidth: 2,
+    //borderBottomColor: '#fff',
+  },
+  tableHeaderText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'white',
+    alignSelf: 'center'
+  },
+  tableRowText: {
+    fontSize: 16,
+    color: 'white',
+    alignSelf: 'center',
+    padding: 15,
+    height: 70,
+    marginTop: 10,
+    marginBottom: -10
+  },
 });
