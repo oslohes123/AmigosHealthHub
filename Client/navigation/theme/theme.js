348d67b07d016b254ecbe0ba5337a5462f3693cc
shazeen123 <shazeenshaheen2003@gmail.com>
2023-02-18 20:08:03 +0000

    Create theme for dark mode

---
+++ Client/navigation/theme/theme.js
@@ -0,0 +1,14 @@
+const theme= {
+    light: {
+        theme: 'light',
+        color: 'black',
+        background: 'white'
+    },
+    dark: {
+        theme: 'dark',
+        color: 'white',
+        background: '#203038'
+    }
+}
+
+export default theme;
