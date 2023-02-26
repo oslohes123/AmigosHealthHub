4a69a3f580bc55c3daeb07aaa9f816a9a9e0dde5
Ashley Tyagi <ashleytyagi@hotmail.com>
2023-02-26 19:51:15 +0000

    Add failed mock tests

--- Server/tests/instantSearch.test.ts
+++ Server/tests/instantSearch.test.ts
@@ -1,34 +1,53 @@
 // Import jest-fetch-mock and instantSearch
-import fetchMock from 'jest-fetch-mock';
+import fetchMock from "jest-fetch-mock";
 import instantSearch from "../searches/instantFoodSearch";
-import {chickenInstantSearchTestData} from "./testdata";
-import { baseUrl } from '../constants';
-import authHeaders from '../searches/headersObject';
+import { chickenInstantSearchTestData } from "./testdata";
+import { baseUrl } from "../constants";
+import authHeaders from "../searches/headersObject";
 
 // Enable fetch mocking
 fetchMock.enableMocks();
 
 // Mock the fetch response with some dummy data
-beforeEach(() => {
+
+describe("instantSearch function", () => {
+  beforeEach(() => {
     fetchMock.mockResponseOnce(JSON.stringify(chickenInstantSearchTestData));
   });
-
-// Write a test for instantSearch
-// Write a test for instantSearch
-test('instantSearch returns a promise with instantSearchInterface', async () => {
+  // Write a test for instantSearch
+  test("instantSearch returns a promise with instantSearchInterface", async () => {
     // Call instantSearch with 'chicken' as argument
-    const result = await instantSearch('chicken');
-  
+    const result = await instantSearch("chicken");
+
     // Expect fetch to have been called once
     expect(fetchMock.mock.calls.length).toEqual(1);
-  
+
     // Expect fetch to have been called with the correct arguments
-    expect(fetchMock.mock.calls[0][0]).toEqual(baseUrl + "search/instant?query=chicken");
+    expect(fetchMock.mock.calls[0][0]).toEqual(
+      baseUrl + "search/instant?query=chicken"
+    );
     expect(fetchMock.mock.calls[0][1]).toEqual({
       method: "GET",
       headers: authHeaders,
     });
-  
+
     // Expect result to match the mock response
     expect(result).toEqual(chickenInstantSearchTestData);
   });
+});
+
+describe("instantSearch false function", () => {
+  // Mock the fetch response with an error
+  beforeEach(() => {
+    fetchMock.mockReject(new Error("Failed to fetch data"));
+  });
+
+  // Write a test for instantSearch that expects an error to be thrown
+  test("returns an error if the fetch fails", async () => {
+    fetchMock.mockReject(new Error("Failed to fetch data"));
+    expect.assertions(1);
+    await expect(instantSearch("chicken")).rejects.toThrow(
+      "Failed to fetch data"
+    );
+  });
+});
--- Server/tests/nutrientSearch.test.ts
+++ Server/tests/nutrientSearch.test.ts
@@ -8,29 +8,47 @@ import { steakNutritionTestData } from "./testdata";
 // Enable fetch mocking
 fetchMock.enableMocks();
 
-// Mock the fetch response with some dummy data
-beforeEach(() => {
-  fetchMock.mockResponseOnce(JSON.stringify(steakNutritionTestData));
-});
-
-// Write a test for nutrientSearch
-test("nutrientSearch returns a promise with nutrientInterface", async () => {
-  // Call nutrientSearch with 'steak' as argument
-  const result = await nutrientSearch("steak");
-
-  // Expect fetch to have been called once
-  expect(fetchMock.mock.calls.length).toEqual(1);
-
-  // Expect fetch to have been called with the correct arguments
-  expect(fetchMock.mock.calls[0][0]).toEqual(baseUrl + "natural/nutrients");
-  expect(fetchMock.mock.calls[0][1]).toEqual({
-    method: "POST",
-    headers: authHeaders,
-    body: new URLSearchParams({
-      query: "steak",
-    }),
+describe("nutrientSearch function test", () => {
+  // Mock the fetch response with some dummy data
+  beforeEach(() => {
+    fetchMock.mockResponseOnce(JSON.stringify(steakNutritionTestData));
   });
 
-  // Expect result to match the mock response
-  expect(result).toEqual(steakNutritionTestData);
+  // Write a test for nutrientSearch
+  test("nutrientSearch returns a promise with nutrientInterface", async () => {
+    // Call nutrientSearch with 'steak' as argument
+    const result = await nutrientSearch("steak");
+
+    // Expect fetch to have been called once
+    expect(fetchMock.mock.calls.length).toEqual(1);
+
+    // Expect fetch to have been called with the correct arguments
+    expect(fetchMock.mock.calls[0][0]).toEqual(baseUrl + "natural/nutrients");
+    expect(fetchMock.mock.calls[0][1]).toEqual({
+      method: "POST",
+      headers: authHeaders,
+      body: new URLSearchParams({
+        query: "steak",
+      }),
+    });
+
+    // Expect result to match the mock response
+    expect(result).toEqual(steakNutritionTestData);
+  });
+});
+
+describe("nutrientSearch false function", () => {
+  // Mock the fetch response with an error
+  beforeEach(() => {
+    fetchMock.mockReject(new Error("Failed to fetch data"));
+  });
+
+  // Write a test for instantSearch that expects an error to be thrown
+  test("returns an error if the fetch fails", async () => {
+    fetchMock.mockReject(new Error("Failed to fetch data"));
+    expect.assertions(1);
+    await expect(nutrientSearch("steak")).rejects.toThrow(
+      "Failed to fetch data"
+    );
+  });
 });
