49561ef20286aaa72de7c52bbe1fbe1d92c0930a
Ashley Tyagi <ashleytyagi@hotmail.com>
2023-02-26 17:54:37 +0000

    Move into seperate to reduce code duplication

--- Server/searches/instantFoodSearch.ts
+++ Server/searches/instantFoodSearch.ts
@@ -1,15 +1,13 @@
 import instantSearchInterface from "../interfaces/instantFoodSearchInterface";
 import {baseUrl} from "../constants";
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import authHeaders from './headersObject';
 dotenv.config()
 
 
 export default async function instantSearch(food: string): Promise<instantSearchInterface> {
     return (await fetch(baseUrl + "search/instant?query=" + food, {
         method: "GET",
-        headers: {
-            "x-app-id": process.env.X_APP_ID as string,
-            "x-app-key": process.env.X_APP_KEY as string,
-        },
+        headers: authHeaders,
     })).json();
 }
