e41736c136d842f2fee1ecd6fb6f0b1dcc8e4d6b
shazeen123 <shazeenshaheen2003@gmail.com>
2023-02-26 23:57:30 +0000

    add opt-out feature toggle to diet settings

--- Client/navigation/screens/Settings/DietSettings.js
+++ Client/navigation/screens/Settings/DietSettings.js
@@ -1,25 +1,44 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native';
+import React, {useState, useContext} from 'react';
+import { View, StyleSheet, Text, SafeAreaView, Switch} from 'react-native';
+import { EventRegister } from 'react-native-event-listeners'
+import themeContext from '../../theme/themeContext';
 
 export default function DietSettings() {
+
+  const theme = useContext(themeContext)
+  const [darkMode, setDarkMode] = useState(false)
+
+  const [showHeader, setShowHeader] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Diet Settings</Text>
-    </View>
+    <SafeAreaView style={[styles.container, {backgroundColor:theme.background}]}>
+      <View style={styles.CalorieView}>
+        <Text style={[styles.text, {color:theme.color}]}>Calories to goal</Text>
+        <Switch
+          value={showHeader}
+          onValueChange={(value) => {
+          setShowHeader(value);
+          EventRegister.emit('ChangeHeader', value)
+          }}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: '#203038',
-      flex: 1,
-    },
-    header: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginTop: 30,
-        color: 'white',
-        alignSelf: 'center',
-      },
+  container: {
+    backgroundColor: '#203038',
+    flex: 1,
+  },
+  CalorieView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: '10%',
+    marginTop: '10%'
+  },
+  text: {
+    fontSize: 30
+},
 })
 
