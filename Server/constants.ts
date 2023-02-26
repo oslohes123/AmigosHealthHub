6eb302027b47c3bbff5023ff547f080a068dd109
Ashley Tyagi <ashleytyagi@hotmail.com>
2023-02-26 17:52:23 +0000

    Update spacings

--- Server/constants.ts
+++ Server/constants.ts
@@ -2,8 +2,8 @@ export const baseUrl = "https://trackapi.nutritionix.com/v2/";
 
 export const enum searchMethods  {
     instantSearch = 0,
-    nutrientSearch= 1,
-    brandedSearch= 2,
+    nutrientSearch = 1,
+    brandedSearch = 2,
 }
 export interface SearchCriteria {
     value:string,
