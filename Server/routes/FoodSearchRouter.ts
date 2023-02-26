cc8de794e20bfc130690fa663f84c634a06896d5
Ashley Tyagi <ashleytyagi@hotmail.com>
2023-02-26 17:54:06 +0000

    Update route

--- Server/routes/FoodSearchRouter.ts
+++ Server/routes/FoodSearchRouter.ts
@@ -2,13 +2,13 @@ const express = require('express');
 const FoodSearchRouter = express.Router();
 FoodSearchRouter.use(express.json());
 // import{checkToken} from '../middleware/checkToken'
-import {generalSearch} from "../controllers/FoodSearchController.ts";
+import {generalSearch} from "../controllers/FoodSearchController";
 
 // FoodSearchRouter.use(checkToken); //Routes are protected.
 /**
  * All these routes start with /edgefunctions/
  */
-FoodSearchRouter.get('/foodsearch/:code.:value',generalSearch)
+FoodSearchRouter.get('/:code.:value',generalSearch)
 
 
 // module.exports = changeProfileDetailsRouter;
