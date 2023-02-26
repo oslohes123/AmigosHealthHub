e85798c25a3650e067166112147ba1db0a7eed49
Ashley Tyagi <ashleytyagi@hotmail.com>
2023-02-26 17:52:14 +0000

    Update spacings

--- Server/searches/brandedSearch.ts
+++ Server/searches/brandedSearch.ts
@@ -1,11 +1,9 @@
 import brandedSearchInterface from '../interfaces/brandedSearchInterface'
 import {baseUrl} from "../constants";
+import authHeaders from './headersObject';
 export default async function brandedSearch(nix_item_id: string): Promise<brandedSearchInterface> {
     return (await fetch(baseUrl + "search/item?nix_item_id=" + nix_item_id, {
         method: "GET",
-        headers: {
-            "x-app-id": process.env.X_APP_ID as string,
-            "x-app-key": process.env.X_APP_KEY as string,
-        },
+        headers: authHeaders,
     })).json();
 }
