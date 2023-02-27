dd68f9d13abebd26b23fc52f6f230bdad3c2dfeb
Ashley Tyagi <ashleytyagi@hotmail.com>
2023-02-27 20:12:01 +0000

    Update foodSearch to return data in our interface

--- Server/controllers/FoodSearchController.ts
+++ Server/controllers/FoodSearchController.ts
@@ -4,6 +4,11 @@ import { SearchCriteria, searchMethods } from "../constants";
 import brandedSearch from "../searches/brandedSearch";
 import instantSearch from "../searches/instantFoodSearch";
 import nutrientSearch from "../searches/nutrientSearch";
+import transformNutrientSearchInterface  from '../utils/parseNutritionSearch';
+import transformBrandedSearchInterface from '../utils/parseBrandedSearch';
+import transformInstantSearchInterface from '../utils/parseInstantSearch';
+import genericSearchInterface from './../interfaces/genericSearchInterface';
+import specificFoodNutritionInterface from './../interfaces/specificFoodNutritionInterface';
 
 
 export const generalSearch = async(req:Request,res:Response) =>{
@@ -11,21 +16,28 @@ export const generalSearch = async(req:Request,res:Response) =>{
     const inputData = req.params.value
     const code = parseInt(req.params.code)
     let data;
+    let result:genericSearchInterface | specificFoodNutritionInterface = {items:[]};
+
     switch (code) {
         case searchMethods.instantSearch:
             data = await instantSearch(inputData)
+            result = transformInstantSearchInterface(data)
             break;
         case searchMethods.nutrientSearch:
             data = await nutrientSearch(inputData)
+            result = transformNutrientSearchInterface(data)
             break;
         case searchMethods.brandedSearch:
             data = await brandedSearch(inputData)
+            result = transformBrandedSearchInterface(data)
             break;
         default:
             data = `The code was incorrect as ${code} use literal searchMethods for correct code`
+            result = {items:[], errorMessage: `Invalid code: ${code}. Use literal searchMethods for correct code.` };
     }
 
-    res.json(data);
+    console.log(result);
+    
+    res.json(result);
 }
-
 
