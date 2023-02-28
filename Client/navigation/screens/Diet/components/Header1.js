413c85033cf1ba9492c2e8480ba49244ef8a726c
shazeen123 <shazeenshaheen2003@gmail.com>
2023-02-28 14:43:49 +0000

    remove header1 content from diet

--- Client/navigation/screens/Diet/components/Header1.js
+++ Client/navigation/screens/Diet/components/Header1.js
@@ -1,39 +1,40 @@
-import React, {useContext} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import themeContext from '../../../theme/themeContext';
+// import React, {useContext} from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
+// import themeContext from '../../../theme/themeContext';
 
-export default function Header1() {
+// // export default function Header1({route}) {
+//   export default function Header1({}) {
+//   const theme = useContext(themeContext)
+//   // const goal = route.getParam('goal')
 
-  const theme = useContext(themeContext)
+//   return (
+//     <View >
+//       <Text style={[styles.title, {color:theme.color}]}>Calorie Goal</Text>
+//       <Text style={[styles.number, {color:theme.color}, {borderColor:theme.color}]}>1000cal</Text>
+//     </View>
+//   );
+// }
 
-  return (
-    <View >
-      <Text style={[styles.title, {color:theme.color}]}>Calorie Goal</Text>
-      <Text style={[styles.number, {color:theme.color}, {borderColor:theme.color}]}>1000cal</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  title: {
-    textAlign: 'center',
-    color: '#fff',
-    fontSize: 25,
-    fontWeight: 'bold',
-    paddingTop: 20,
-    paddingBottom: 10,
-    marginTop: 1,
-  },
-  number: {
-    textAlign: 'center',
-    color: '#fff',
-    fontSize: 25,
-    fontWeight: 'bold',
-    paddingTop: 10,
-    borderStyle: 'solid',
-    borderWidth: 2,
-    paddingBottom: 5,
-    width: '30%',
-    alignSelf: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   title: {
+//     textAlign: 'center',
+//     color: '#fff',
+//     fontSize: 25,
+//     fontWeight: 'bold',
+//     paddingTop: 20,
+//     paddingBottom: 10,
+//     marginTop: 1,
+//   },
+//   number: {
+//     textAlign: 'center',
+//     color: '#fff',
+//     fontSize: 25,
+//     fontWeight: 'bold',
+//     paddingTop: 10,
+//     borderStyle: 'solid',
+//     borderWidth: 2,
+//     paddingBottom: 5,
+//     width: '30%',
+//     alignSelf: 'center',
+//   },
+// });
