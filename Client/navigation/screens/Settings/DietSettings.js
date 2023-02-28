c3acf30798cc64baeeffe0574b9633ef41180df5
shazeen123 <shazeenshaheen2003@gmail.com>
2023-02-28 21:46:38 +0000

    add cal goal changing input text to diet settings

--- Client/navigation/screens/Settings/DietSettings.js
+++ Client/navigation/screens/Settings/DietSettings.js
@@ -1,11 +1,25 @@
 import React, {useState, useContext} from 'react';
-import { View, StyleSheet, Text, SafeAreaView, Switch} from 'react-native';
+import { View, StyleSheet, Text, SafeAreaView, Switch, TextInput} from 'react-native';
 // import { EventRegister } from 'react-native-event-listeners'
 import themeContext from '../../theme/themeContext';
+import GreenButton from '../../components/GreenButton';
 
 export default function DietSettings() {
 
   const theme = useContext(themeContext)
+
+  const [goal, setGoal] = useState('');
+
+  const handleButtonPress = () => {
+    if (goal === '') {
+      alert('Please enter new calorie goal');
+    } else if (isNaN(goal)) {
+        alert('Calorie should be a number');
+    } else {
+        setGoal('');
+        alert('Calorie goal successfully added');
+    }
+  }
   // const [darkMode, setDarkMode] = useState(false)
 
   // const [showHeader, setShowHeader] = useState(false)
@@ -22,6 +36,19 @@ export default function DietSettings() {
           }}
         />
       </View> */}
+
+      <View>
+        <TextInput
+          placeholder='Add new calorie goal'
+          placeholderTextColor={theme.color}
+          style={[styles.input, {borderColor: theme.color}, {color: theme.color}]}
+          keyboardType="numeric"
+          value={goal}
+          onChangeText={setGoal}
+          clearButtonMode='always'
+        />
+        <GreenButton text='Set Goal' buttonFunction={handleButtonPress} height={60} width={220}/>
+      </View>
     </SafeAreaView>
   )
 }
@@ -31,6 +58,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#203038',
     flex: 1,
   },
+  input: {
+    alignSelf: 'center',
+    marginTop: '15%',
+    fontSize: 16,
+    borderWidth: 1,
+    padding: '3%',
+    width: '70%',
+    borderRadius: 25,
+}
   // CalorieView: {
   //   flexDirection: 'row',
   //   justifyContent: 'space-between',
