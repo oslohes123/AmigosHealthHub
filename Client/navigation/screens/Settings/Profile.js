3b76e9c87313e80d1d2f8b1c323548fd0fb43c7e
shazeen123 <shazeenshaheen2003@gmail.com>
2023-02-28 23:24:34 +0000

    edit UI of profile page

--- Client/navigation/screens/Settings/Profile.js
+++ Client/navigation/screens/Settings/Profile.js
@@ -15,12 +15,33 @@ export default function Profile() {
         <Ionicons name={'person-circle-outline'} size={80} color={theme.color} />
       </View>
       
-      <View>   
-        <Text style={[styles.text, {color:theme.color}]}>Name</Text>
-        <Text style={[styles.text, {color:theme.color}]}>Email</Text>
-        <Text style={[styles.text, {color:theme.color}]}>Phone</Text>
-        <Text style={[styles.text, {color:theme.color}]}>Weight</Text>
-        <Text style={[styles.text, {color:theme.color}]}>Height</Text>
+      <View> 
+
+        <View style={styles.details}>  
+          <Text style={[styles.head, {color:theme.color}]}>Name</Text>
+          <Text style={[styles.body, {color:theme.color}]}>abcdefg</Text>
+        </View>
+
+        <View style={styles.details}> 
+          <Text style={[styles.head, {color:theme.color}]}>Email</Text>
+          <Text style={[styles.body, {color:theme.color}]}>abcd@mail.com</Text>
+        </View>
+
+        <View style={styles.details}> 
+          <Text style={[styles.head, {color:theme.color}]}>Phone</Text>
+          <Text style={[styles.body, {color:theme.color}]}>+44   712345678</Text>
+        </View>
+
+        <View style={styles.details}> 
+          <Text style={[styles.head, {color:theme.color}]}>Weight</Text>
+          <Text style={[styles.body, {color:theme.color}]}>60kg</Text>
+        </View>
+        
+        <View style={styles.details}> 
+          <Text style={[styles.head, {color:theme.color}]}>Height</Text>
+          <Text style={[styles.body, {color:theme.color}]}>160cm</Text>
+        </View>
+
       </View>
     </SafeAreaView>
   )
@@ -34,9 +55,15 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       margin: '7%'
     },
-    text: {
+    head: {
+      fontSize: 20,
+      fontWeight: 'bold'
+    },
+    details: {
+      margin: '5%'
+    },
+    body: {
       fontSize: 30,
-      margin: '8%'
     }
 
 })
