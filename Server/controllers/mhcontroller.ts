cb8ea8603460c7fdad1b6a7ee196009e916835ce
V1SHAL421 <70663959+V1SHAL421@users.noreply.github.com>
2023-03-03 22:22:02 +0000

    Amend routes

--- Server/controllers/mhcontroller.ts
+++ Server/controllers/mhcontroller.ts
@@ -27,14 +27,27 @@ export const mainPage = async(req:Request,res:Response) => {
     //     else{
     //         const worddata = data
     //     }
+    let words: any
+    let faces: any
         try{
-            const words = await returnWords() 
+            words = await returnWords() 
             console.log(JSON.stringify(words))
-            return res.status(200).json({mssg: "Words retrieved"})
+            // return res.status(200).json({mssg: "Words retrieved", 'words': words})
+        }
+        catch(Error: unknown){
+            return res.status(400).json({mssg:"Failed to retrieve last 7 words"})
+        }
+
+        try{
+            faces = await returnFaces() 
+            console.log(JSON.stringify(faces))
+            // return res.status(200).json({mssg: "Words retrieved", 'faces': faces})
         }
         catch(Error: unknown){
             return res.status(400).json({mssg:"Failed to retrieve last 7 faces"})
         }
+
+        return res.status(200).json({mssg: "Retrieved words", words: await returnWords(), faces: await returnFaces()})
     }
         
         // const { data,error }:any = await supabaseQuery.mostrecent(supabase, 'Mental Health','face_id','created_at');
