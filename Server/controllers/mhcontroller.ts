522d81aac71c46c3e3b535cf99aa6f8535e46088
V1SHAL421 <70663959+V1SHAL421@users.noreply.github.com>
2023-03-06 17:54:15 +0000

    Retrieve tests for other components

--- Server/controllers/mhcontroller.ts
+++ Server/controllers/mhcontroller.ts
@@ -1,23 +1,26 @@
 require('dotenv').config()
+
 import { Request, Response } from 'express';
+import { arrayOfObjectsToStrings, average, getFaces, getOccurrences, getWords, returnFaces, returnWords, wordFreq } from '../functions/mhfunctions'
+
 import supabase from '../utils/supabaseSetUp'
 import {supabaseQueryClass} from '../utils/databaseInterface'
-import { returnWords, returnFaces, arrayOfObjectsToStrings, getWords, getFaces, average, getOccurrences, wordFreq} from '../functions/mhfunctions'
+
 const supabaseQuery = new supabaseQueryClass();
 
-async function getLastSevenWords(): Promise<object>{
-    const { data,error }:any = await supabaseQuery.mostrecent(supabase, 'Mental Health','MH_ID','created_at');
-    if(error){
-        console.log("Failed to return last 7 words")
-        return { mssg:"Failed to retrieve last 7 words" }
-        // return res.status(400).json({mssg:"Failed to delete account!"})
-    }
-    else{
-        console.log("Words returned")
-        return { data }
-    }
-}
-export const wordValues = async(req:Request,res:Response) => {
+// async function getLastSevenWords(): Promise<object>{
+//     const { data,error }:any = await supabaseQuery.mostrecent(supabase, 'Mental Health','MH_ID','created_at');
+//     if(error){
+//         console.log("Failed to return last 7 words")
+//         return { mssg:"Failed to retrieve last 7 words" }
+//         // return res.status(400).json({mssg:"Failed to delete account!"})
+//     }
+//     else{
+//         console.log("Words returned")
+//         return { data }
+//     }
+// }
+export const mainPage = async(req:Request,res:Response) => {
     // return res.status(200).json({mssg: "MentalHealthOverview"})
     // const { data,error }:any = await supabaseQuery.mostrecent(supabase, 'Mental Health','todays_word','created_at');
     //     if(error){
@@ -28,7 +31,7 @@ export const wordValues = async(req:Request,res:Response) => {
     //         const worddata = data
     //     }
     let words: any
-
+    let faces: any
         try{
             words = await returnWords() 
             console.log(JSON.stringify(words))
@@ -38,25 +41,22 @@ export const wordValues = async(req:Request,res:Response) => {
             return res.status(400).json({mssg:"Failed to retrieve last 7 words"})
         }
 
-        return res.status(200).json({mssg: "Retrieved words", words:words, faces: faces, success: "successful"}) 
+        try{
+            faces = await returnFaces() 
+            console.log(JSON.stringify(faces))
+            // return res.status(200).json({mssg: "Words retrieved", 'faces': faces})
+        }
+        catch(Error: unknown){
+            return res.status(400).json({mssg:"Failed to retrieve last 7 faces"})
+        }
+
+        return res.status(200).json({mssg: "Retrieved words", words:words, faces: await returnFaces()}) 
     }
         
         // const { data,error }:any = await supabaseQuery.mostrecent(supabase, 'Mental Health','face_id','created_at');
         // if(error){
         //     console.log("Failed to return last 7 faces")
         //     return {mssg : "Failed to return last 7 faces"}
-export const faceValues = async(req:Request,res:Response) =>{
-    let faces: any
-    
-    try{
-        faces = await returnFaces() 
-        console.log(JSON.stringify(faces))
-        // return res.status(200).json({mssg: "Words retrieved", 'faces': faces})
-    }
-    catch(Error: unknown){
-        return res.status(400).json({mssg:"Failed to retrieve last 7 faces"})
-    }
-}
         
         
  
@@ -66,4 +66,6 @@ export const faceValues = async(req:Request,res:Response) =>{
 export const reviewDay = async(req:Request,res:Response) => {
     return res.status(400).json({mssg:"Failed to retrieve last 7 faces"})
 }
+// take face and name from req.body
+
 // take face and name from req.body
--- Server/tests/mhfunctions.test.ts
+++ Server/tests/mhfunctions.test.ts
@@ -1,92 +1,92 @@
-// import sinon from 'sinon';
+// // import sinon from 'sinon';
 
-import test from 'ava';
-import { average, getOccurrences, wordFreq } from '../functions/mhfunctions';
+// import test from 'ava';
+// import { average, getOccurrences, wordFreq } from '../functions/mhfunctions';
 
-test('average should return the correct value for an array of numbers', t => {
-  const arr: number[] = [1, 2, 3, 4, 5];
-  const expected: number = 3;
-  const actual: number = average(arr);
-  t.is(actual, expected);
-});
+// test('average should return the correct value for an array of numbers', t => {
+//   const arr: number[] = [1, 2, 3, 4, 5];
+//   const expected: number = 3;
+//   const actual: number = average(arr);
+//   t.is(actual, expected);
+// });
 
-test('average should return 0 for an empty array', t => {
-  const arr: number[] = [];
-  const expected: number = 0;
-  const actual: number = average(arr);
-  t.is(actual, expected);
-});
+// test('average should return 0 for an empty array', t => {
+//   const arr: number[] = [];
+//   const expected: number = 0;
+//   const actual: number = average(arr);
+//   t.is(actual, expected);
+// });
 
-test('average should handle negative numbers', t => {
-  const arr: number[] = [-1, -2, -3, -4, -5];
-  const expected: number = -3;
-  const actual: number = average(arr);
-  t.is(actual, expected);
-});
+// test('average should handle negative numbers', t => {
+//   const arr: number[] = [-1, -2, -3, -4, -5];
+//   const expected: number = -3;
+//   const actual: number = average(arr);
+//   t.is(actual, expected);
+// });
 
-test('average should handle decimal numbers', t => {
-  const arr: number[] = [1.5, 2.5, 3.5, 4.5, 5.5];
-  const expected: number = 3.5;
-  const actual: number = average(arr);
-  t.is(actual, expected);
-});
+// test('average should handle decimal numbers', t => {
+//   const arr: number[] = [1.5, 2.5, 3.5, 4.5, 5.5];
+//   const expected: number = 3.5;
+//   const actual: number = average(arr);
+//   t.is(actual, expected);
+// });
 
-test('getOccurrences should return 0 when the value is not found in the array', t => {
-    const arr: string[] = ['hello', 'bar', 'baz'];
-    const v:string = 'qux';
-    const expected: number = 0;
-    const actual = getOccurrences(arr, v);
-    t.is(actual, expected);
-  });
+// test('getOccurrences should return 0 when the value is not found in the array', t => {
+//     const arr: string[] = ['hello', 'bar', 'baz'];
+//     const v:string = 'qux';
+//     const expected: number = 0;
+//     const actual = getOccurrences(arr, v);
+//     t.is(actual, expected);
+//   });
   
-  test('getOccurrences should return the correct count when the value is found in the array', t => {
-    const arr: string[] = ['hello', 'bar', 'baz', 'bar', 'baz'];
-    const v = 'bar';
-    const expected: number = 2;
-    const actual = getOccurrences(arr, v);
-    t.is(actual, expected);
-  });
+//   test('getOccurrences should return the correct count when the value is found in the array', t => {
+//     const arr: string[] = ['hello', 'bar', 'baz', 'bar', 'baz'];
+//     const v = 'bar';
+//     const expected: number = 2;
+//     const actual = getOccurrences(arr, v);
+//     t.is(actual, expected);
+//   });
   
-  test('getOccurrences should handle case-sensitive values', t => {
-    const arr: string[] = ['hello', 'Bar', 'baz', 'BAR', 'bAz'];
-    const v: string = 'Bar';
-    const expected: number = 1;
-    const actual = getOccurrences(arr, v);
-    t.is(actual, expected);
-  });
+//   test('getOccurrences should handle case-sensitive values', t => {
+//     const arr: string[] = ['hello', 'Bar', 'baz', 'BAR', 'bAz'];
+//     const v: string = 'Bar';
+//     const expected: number = 1;
+//     const actual = getOccurrences(arr, v);
+//     t.is(actual, expected);
+//   });
   
-  test('getOccurrences should handle special characters', t => {
-    const arr: string[] = ['hello', 'bar', 'baz', '@', '@', '@'];
-    const v: string = '@';
-    const expected: number = 3;
-    const actual = getOccurrences(arr, v);
-    t.is(actual, expected);
-  });
+//   test('getOccurrences should handle special characters', t => {
+//     const arr: string[] = ['hello', 'bar', 'baz', '@', '@', '@'];
+//     const v: string = '@';
+//     const expected: number = 3;
+//     const actual = getOccurrences(arr, v);
+//     t.is(actual, expected);
+//   });
 
-  test('wordFreq should return an empty array when passed an empty array', t => {
-    const arr: string[] = [];
-    const expected: string[] = [];
-    const actual = wordFreq(arr);
-    t.deepEqual(actual, expected);
-  });
+//   test('wordFreq should return an empty array when passed an empty array', t => {
+//     const arr: string[] = [];
+//     const expected: string[] = [];
+//     const actual = wordFreq(arr);
+//     t.deepEqual(actual, expected);
+//   });
   
-  test('wordFreq should return an array of strings when passed an array of strings', t => {
-    const arr: string[] = ['hello', 'bar', 'baz', 'hello', 'qux', 'bar', 'quux'];
-    const expected: string[] = ['hello', '2', 'bar', '2', 'baz', '1', 'qux', '1', 'quux', '1'];
-    const actual = wordFreq(arr);
-    t.deepEqual(actual, expected);
-  });
+//   test('wordFreq should return an array of strings when passed an array of strings', t => {
+//     const arr: string[] = ['hello', 'bar', 'baz', 'hello', 'qux', 'bar', 'quux'];
+//     const expected: string[] = ['hello', '2', 'bar', '2', 'baz', '1', 'qux', '1', 'quux', '1'];
+//     const actual = wordFreq(arr);
+//     t.deepEqual(actual, expected);
+//   });
   
-  test('wordFreq should handle case-sensitive values', t => {
-    const arr: string[] = ['hello', 'Bar', 'baz', 'BAR', 'bAz'];
-    const expected: string[] = ['hello', '1', 'Bar', '1', 'baz', '1', 'BAR', '1', 'bAz', '1'];
-    const actual = wordFreq(arr);
-    t.deepEqual(actual, expected);
-  });
+//   test('wordFreq should handle case-sensitive values', t => {
+//     const arr: string[] = ['hello', 'Bar', 'baz', 'BAR', 'bAz'];
+//     const expected: string[] = ['hello', '1', 'Bar', '1', 'baz', '1', 'BAR', '1', 'bAz', '1'];
+//     const actual = wordFreq(arr);
+//     t.deepEqual(actual, expected);
+//   });
   
-  test('wordFreq should handle special characters', t => {
-    const arr: string[] = ['hello', 'bar', 'baz', '@', '@', '@'];
-    const expected: string[] = ['hello', '1', 'bar', '1', 'baz', '1', '@', '3'];
-    const actual = wordFreq(arr);
-    t.deepEqual(actual, expected);
-  });
+//   test('wordFreq should handle special characters', t => {
+//     const arr: string[] = ['hello', 'bar', 'baz', '@', '@', '@'];
+//     const expected: string[] = ['hello', '1', 'bar', '1', 'baz', '1', '@', '3'];
+//     const actual = wordFreq(arr);
+//     t.deepEqual(actual, expected);
+//   });
