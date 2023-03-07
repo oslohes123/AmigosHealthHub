873ad9ce97a5bec84060aa544fe4bfd3fc6ceef5
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-07 16:15:53 +0000

    create calendar and display food history

--- Client/navigation/screens/Diet/FoodHistory.js
+++ Client/navigation/screens/Diet/FoodHistory.js
@@ -1,10 +1,58 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react'
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Calendar } from 'react-native-calendars'
+import { date } from 'yup';
+import GreenButton from '../../components/GreenButton';
 
 export default function FoodHistory() {
+
+  const[viewCalendar, setViewCalendar] = useState(false);
+  const[selectDay, setSelectDay] = useState(null);
+  const[food, setFood] = useState('');
+
+  const toggleCalendar = () => {
+    setViewCalendar(!viewCalendar);
+  }
+
+  const handleDayPress = (day) => {
+    setSelectDay(day.dateString);
+    console.log('selected day', day);
+    setViewCalendar(false);
+    setFood(getFood(day.dateString));
+  }
+
+  const getFood = (dateString) => {
+    if (dateString === '2023-03-02') {
+      return 'Apple';
+    } else if (dateString === '2023-03-03') {
+      return 'Banana';
+    } else if (dateString === '2023-03-04') {
+      return 'Orange';
+    } else {
+      return 'No food item consumed on this day';
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Food Consumed</Text>
+      <View style={styles.primary}>
+        {selectDay && (
+          <Text style={styles.text}>{selectDay}</Text>
+        )}
+        <GreenButton text='View Calender' height={70} buttonFunction={toggleCalendar}/>
+      </View>
+      {viewCalendar && (
+        <Calendar
+        style={{width: '90%', alignSelf: 'center'}}
+        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+        onDayPress={handleDayPress}
+        maxDate={new Date()}
+        />
+      )}
+        {!viewCalendar && food !== '' && (
+          <Text style={styles.foodText}>{food}</Text>
+        )}
     </View>
   )
 }
@@ -15,12 +63,40 @@ const styles = StyleSheet.create({
       flex: 1,
     },
     header: {
-        //width: 300,
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginTop: 30,
-        color: 'white',
-        alignSelf: 'center',
+      //width: 300,
+      fontSize: 30,
+      fontWeight: 'bold',
+      marginTop: 30,
+      color: 'white',
+      alignSelf: 'center',
       },
+    primary: {
+      flexDirection:'row', 
+      alignItems: 'center', 
+      justifyContent: 'space-between',
+      marginHorizontal: '10%',
+      marginVertical: '5%',
+    },
+    text: {
+      fontSize: 20,
+      color: 'white',
+      fontWeight: 'bold',
+      //backgroundColor: 'white',
+      padding: 10,
+      borderRadius: 15,
+      borderWidth: 1,
+      borderColor: 'white'
+    },
+    foodText: {
+      fontSize: 20,
+      color: 'white',
+      fontWeight: 'bold',
+      //backgroundColor: 'white',
+      padding: 10,
+      borderRadius: 15,
+      borderWidth: 1,
+      borderColor: 'white',
+      alignSelf: 'center'
+    }
 })
 
