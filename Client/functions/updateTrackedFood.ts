b7b2555f2a13db5ef4df43d344fcf919ea3d244e
Ashley <ashleytyagi@hotmail.com>
2023-03-08 09:07:49 +0000

    Build post reqeust to send data to server using axios

--- Client/functions/updateTrackedFood.ts
+++ Client/functions/updateTrackedFood.ts
@@ -1,22 +1,34 @@
 import axios, { AxiosResponse, AxiosError } from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const portENV = process.env["PORT"];
 const ip_addressENV = process.env["IP_ADDRESS"];
 // For testing purposes
 // Update this with your own UrlService
-let ip_address:string|undefined = ip_addressENV;
-let port:string|undefined= portENV
+let ip_address: string | undefined = ip_addressENV;
+let port: string | undefined = portENV
 
-export async function addTrackedFood(food:JSON) {
-    let url:string = `http://${ip_address}:${port}/api/food/updateTrackedFood`;
-    let response:AxiosResponse;
+export async function addTrackedFood(food: JSON) {
+    let url: string = `http://${ip_address}:${port}/api/food/updateTrackedFood`;
+    let response: AxiosResponse;
     try {
-        response = await axios.post(url, food);
+        const { token } = JSON.parse(
+            (await AsyncStorage.getItem('user')) as string
+        );
+        response = await axios.post(url, {
+            food: food,
+        },{
+            headers: {
+                authorization: token
+            }
+        });
     }
     catch (error: any | AxiosError) {
-        if(axios.isAxiosError(error)){
+        if (axios.isAxiosError(error)) {
+            console.log("Got an error from the server");
             console.log(error.response);
-        }else{
+        } else {
             console.log("Default error handler" + error);
         }
         return error;
