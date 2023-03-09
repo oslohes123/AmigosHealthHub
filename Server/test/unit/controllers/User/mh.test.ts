71913f6da67f84e780f3ca2108b51be18316a076
V1SHAL421 <70663959+V1SHAL421@users.noreply.github.com>
2023-03-09 12:23:16 +0000

    Unit test mental health backend routes

--- Server/test/unit/controllers/User/mh.test.ts
+++ Server/test/unit/controllers/User/mh.test.ts
@@ -1,6 +1,7 @@
+const test = require('ava');
 import { Request, Response } from 'express';
-import sinon from "sinon";
-import { faceValues } from "../../../../controllers/mhcontroller";
+const sinon = require('sinon');
+import { faceValues, wordValues } from "../../../../controllers/mhcontroller";
 import { supabaseQueryClass } from '../../../../utils/databaseInterface';
 import supabase from '../../../../utils/supabaseSetUp';
 
@@ -18,15 +19,51 @@ const mockRequest = (sessionData: any) => {
         body: sessionData
     }
 }
-// test("Return last 7 words and their frequencies", async (t: any) => {
+test("Return last 7 words and their frequencies", async (t: any) => {
 
-//     console.log("In returning last 7 words and their frequencies")
-//     const req = mockRequest({id: '9d-1852-4c2d-802c-e10d3ebdc05b'});
-//     const res = mockResponse();
-//         await faceValues(req as Request, res as Response)
+    console.log("In returning last 7 words and their frequencies")
+    const req = mockRequest({
+        id: '9d-1852-4c2d-802c-e10d3ebdc05b',
+    });
+    const res = mockResponse();
+    await wordValues(req as Request, res as Response)
 
-//         t.true(res.status.calledWith(400))
-//         t.true(res.json.calledWith({mssg : "Failed to return last 7 faces"}))
-// })
+    t.true(res.status.calledWith(400))
+    t.true(res.json.calledWith({mssg : "Failed to return last 7 words"}));
+});
+test("Return last 7 faces and their frequencies", async (t: any) => {
 
-test("Return last 7 faces")
+    console.log("In returning last 7 words and their average")
+    const req = mockRequest({
+        id: '9d-1852-4c2d-802c-e10d3ebdc05b',
+    });
+    const res = mockResponse();
+    await wordValues(req as Request, res as Response)
+
+    t.true(res.status.calledWith(200))
+});
+test("Return last 7 words and their frequencies", async (t: any) => {
+
+    console.log("In returning last 7 words and their frequencies")
+    const req = mockRequest({
+        id: 'e9a8a99d-1852-4c2d-802c-e10d3ebdc05b',
+    });
+    const res = mockResponse();
+    await wordValues(req as Request, res as Response)
+
+    t.true(res.status.calledWith(400))
+    t.true(res.json.calledWith({mssg : "Failed to return last 7 words"}));
+});
+
+test("Return last 7 faces", async (t: any) => {
+
+    console.log("In returning last 7 faces and their average")
+    const req = mockRequest({
+        id: 'e9a8a99d-1852-4c2d-802c-e10d3ebdc05b',
+    });
+    const res = mockResponse();
+    await faceValues(req as Request, res as Response)
+
+    t.true(res.status.calledWith(200))
+
+})
