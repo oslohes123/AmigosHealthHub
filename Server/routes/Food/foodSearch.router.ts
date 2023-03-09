e33915b956700e67eac835617b9b3c8e3de16dbe
Ashley <ashleytyagi@hotmail.com>
2023-03-09 14:01:40 +0000

    Refactor routes

--- Server/routes/Food/foodSearch.router.ts
+++ Server/routes/Food/foodSearch.router.ts
@@ -1,19 +1,16 @@
+import { checkToken} from "../../middleware/checkToken";
+import {generalSearch} from "./foodSearch.controller";
+import routeNames from "../../utils/routeNamesClass";
+
 const express = require('express');
 const FoodSearchRouter = express.Router();
 FoodSearchRouter.use(express.json());
-import { checkToken,checkingTokens } from "../../middleware/checkToken";
-import {generalSearch} from "./foodSearch.controller";
-import routeNames from "../../utils/routeNamesClass";
-let routeNamesClass = new routeNames();
 
-if(checkingTokens){
-    FoodSearchRouter.use(checkToken); //Routes are protected.
-}
+let routeNamesClass = new routeNames();
+FoodSearchRouter.use(checkToken); //Routes are protected.
 
 // All these routes start with /api/food/
-
 FoodSearchRouter.get(routeNamesClass.partialFoodSearchURL,generalSearch)
-
 
 export default FoodSearchRouter;
 export {};
--- Server/routes/Food/updateTrackedFood.router.ts
+++ Server/routes/Food/updateTrackedFood.router.ts
@@ -1,18 +1,17 @@
+import { checkToken } from '../../middleware/checkToken'
+import RouteNamesClass from '../../utils/routeNamesClass';
+import { foodUpdate } from './updateTrackedFood.controller';
+
 const express = require('express');
 const FoodUpdateRouter = express.Router();
 FoodUpdateRouter.use(express.json());
-import{checkToken,checkingTokens} from '../../middleware/checkToken'
-import RouteNamesClass from '../../utils/routeNamesClass';
-import { foodUpdate } from './updateTrackedFood.controller';
 let routeNames = new RouteNamesClass();
 
-if(checkingTokens){
-    FoodUpdateRouter.use(checkToken); //Routes are protected.
-}
+FoodUpdateRouter.use(checkToken); //Routes are protected.
 
 // This route goes to /api/food/updateTrackedFood
 FoodUpdateRouter.post(routeNames.partialFoodUpdateURL, foodUpdate)
 
 // module.exports = changeProfileDetailsRouter;
 export default FoodUpdateRouter;
-export {};
+export { };
