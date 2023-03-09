b4ebfe99de2507333945fe98109c5b69908832ee
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-09 17:45:16 +0000

    remove table data

--- Client/navigation/screens/Diet/Nutrients.js
+++ Client/navigation/screens/Diet/Nutrients.js
@@ -1,34 +1,9 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-// import { Table, Row, Rows } from 'react-native-table-component';
 
 export default function Nutrients() {
 
-//   const header = ['Nutrient', 'Amount'];
-//   const data = [
-//     ['Protein', '82g'],
-//     ['Carbs', '65g'],
-//     ['Fat', '22g'],
-//     ['Energy', '46g'],
-//     ['Sugars', '15g'],
-//     ['Fibre', '49g'],
-//     ['Vitamins', '60g'],
-// ];
-
   return (
-    // <View style={styles.container}>
-    //   <View style={styles.tableContainer}>
-    //     <Text style={styles.header}>Food Consumed</Text>
-    //     <Table borderStyle={styles.tableBorder}>
-    //       <Row
-    //         data={header}
-    //         style={styles.tableHeader}
-    //         textStyle={styles.tableHeaderText}
-    //       />
-    //       <Rows data={data} textStyle={styles.tableRowText} />
-    //     </Table>
-    //   </View>
-    // </View>
     <View style={styles.container}>
       <Text style={styles.header}>Food Consumed</Text>
       
@@ -81,51 +56,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#203038',
     flex: 1,
   },
-  // tableContainer: {
-  //   marginTop: 40,
-  //   alignSelf: 'center',
-  //   width: 350
-  // },
   header: {
-    //width: 300,
     fontSize: 30,
     fontWeight: 'bold',
-    //marginBottom: 15,
     color: 'white',
     alignSelf: 'center',
     marginVertical: '5%'
   },
-  // tableBorder: {
-  //   borderWidth: 2,
-  //   borderColor: 'white',
-  // },
-  // tableHeader: {
-  //   //backgroundColor: '#f2f2f2',
-  //   height: 70,
-  //   //borderBottomWidth: 2,
-  //   //borderBottomColor: '#fff',
-  // },
-  // tableHeaderText: {
-  //   fontSize: 25,
-  //   fontWeight: 'bold',
-  //   color: 'white',
-  //   alignSelf: 'center'
-  // },
-  // tableRowText: {
-  //   fontSize: 16,
-  //   color: 'white',
-  //   alignSelf: 'center',
-  //   padding: 15,
-  //   height: 70,
-  //   marginTop: 10,
-  //   marginBottom: -10
-  // },
   primaryHeader: {
     fontSize: 25,
     color: 'white',
     borderWidth: 2,
     borderColor: 'white',
-    //padding: '8%'
     width: '70%',
     paddingVertical: '13%',
   },
