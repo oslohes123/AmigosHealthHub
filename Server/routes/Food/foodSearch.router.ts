7de52387dc1622c4221ae2adb4eed5927afa26c3
Ashley <ashleytyagi@hotmail.com>
2023-03-09 08:49:20 +0000

    Use checkingTokens

--- Server/routes/Food/foodSearch.router.ts
+++ Server/routes/Food/foodSearch.router.ts
@@ -1,10 +1,14 @@
 const express = require('express');
 const FoodSearchRouter = express.Router();
 FoodSearchRouter.use(express.json());
-import { checkToken } from "../../middleware/checkToken";
+import { checkToken,checkingTokens } from "../../middleware/checkToken";
 import {generalSearch} from "./foodSearch.controller";
 import routeNames from "../../utils/routeNamesClass";
 let routeNamesClass = new routeNames();
+
+if(checkingTokens){
+    FoodSearchRouter.use(checkToken); //Routes are protected.
+}
 
 // FoodSearchRouter.use(checkToken); //Routes are protected.
 
