7b7c502f1397b3bbebd925ceec49d91c09015d2b
Ashley <ashleytyagi@hotmail.com>
2023-03-09 14:12:23 +0000

    Fix calendar max date issue

--- Client/navigation/screens/Diet/FoodHistory.js
+++ Client/navigation/screens/Diet/FoodHistory.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 import { View, StyleSheet, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { Calendar } from 'react-native-calendars'
 import { date } from 'yup';
-import { AntDesign } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons';
 import { PieChart } from "react-native-chart-kit";
 
-export default function FoodHistory({navigation}) {
+export default function FoodHistory({ navigation }) {
 
-  const foodData ={
+  const foodData = {
     '2023-03-08': [
       {
         name: "Apple",
@@ -47,7 +47,7 @@ export default function FoodHistory({navigation}) {
       },
     ],
   }
-  
+
   const Piedata = [
     {
       name: "Protein",
@@ -86,9 +86,9 @@ export default function FoodHistory({navigation}) {
     }
   ];
 
-  const[viewCalendar, setViewCalendar] = useState(false);
-  const[selectDay, setSelectDay] = useState(null);
-  const[food, setFood] = useState('');
+  const [viewCalendar, setViewCalendar] = useState(false);
+  const [selectDay, setSelectDay] = useState(null);
+  const [food, setFood] = useState('');
 
   const toggleCalendar = () => {
     setViewCalendar(!viewCalendar);
@@ -119,91 +119,92 @@ export default function FoodHistory({navigation}) {
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scroll}>
-      <View style={styles.primary}>
-        {!selectDay && (
-          <Text style={styles.text}>Select a day from the Calendar to View Food History</Text>
+        <View style={styles.primary}>
+          {!selectDay && (
+            <Text style={styles.text}>Select a day from the Calendar to View Food History</Text>
+          )}
+          {selectDay && (
+            <Text style={styles.text}>{selectDay}-: 2500cal consumed</Text>
+          )}
+          <TouchableOpacity style={styles.icon} onPress={toggleCalendar}>
+            <AntDesign name="calendar" size={35} color="white" />
+          </TouchableOpacity>
+        </View>
+
+        {selectDay && !viewCalendar && (
+          <TouchableOpacity style={styles.pieWidget} onPress={pressHandler}>
+            <PieChart
+              data={Piedata}
+              width={340}
+              height={210}
+              paddingLeft='10'
+              chartConfig={{
+                color: () => "black",
+              }}
+              accessor="amount"
+              backgroundColor="transparent"
+            />
+          </TouchableOpacity>
         )}
-        {selectDay && (
-          <Text style={styles.text}>{selectDay}-: 2500cal consumed</Text>
-        )}
-      <TouchableOpacity style={styles.icon} onPress={toggleCalendar}>
-        <AntDesign name="calendar" size={35} color="white" />
-      </TouchableOpacity>
-      </View>
-      {selectDay && !viewCalendar &&(
-      <TouchableOpacity style={styles.pieWidget} onPress={pressHandler}>
-        <PieChart
-          data={Piedata}
-          width={340}
-          height={210}
-          paddingLeft='10'
-          chartConfig={{
-            color: () => "black",
-          }}
-          accessor="amount"
-          backgroundColor="transparent"
+        {viewCalendar && (
+          <Calendar
+            style={{ width: '90%', alignSelf: 'center' }}
+            onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
+            onDayPress={handleDayPress}
+            maxDate={new Date().toISOString().split('T')[0]}
           />
-        </TouchableOpacity>
-      )}
-      {viewCalendar && (
-        <Calendar
-        style={{width: '90%', alignSelf: 'center'}}
-        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
-        onDayPress={handleDayPress}
-        maxDate={new Date()}
-        />
-      )}
+        )}
         {!viewCalendar && food !== '' && (
           <Text style={styles.foodText}>{food}</Text>
         )}
-    </ScrollView>    
+      </ScrollView>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: '#203038',
-      flex: 1,
-    },
-    primary: {
-      flexDirection:'row', 
-      alignItems: 'center', 
-      justifyContent: 'space-between',
-      marginHorizontal: '15%',
-      marginVertical: '5%',
-    },
-    text: {
-      fontSize: 20,
-      color: 'white',
-      fontWeight: 'bold',
-      padding: 10,
-      borderRadius: 15,
-      borderWidth: 1,
-      borderColor: 'white'
-    },
-    foodText: {
-      fontSize: 20,
-      color: 'white',
-      fontWeight: 'bold',
-      padding: 10,
-      borderRadius: 15,
-      borderWidth: 1,
-      borderColor: 'white',
-      alignSelf: 'center',
-      marginVertical: '5%'
-    },
-    icon: {
-      marginLeft: '4%'
-    },
-    pieWidget: {
-      backgroundColor: '#3eda9b',
-      borderRadius: 25,
-      alignSelf: 'center',
-      padding: 5
-    },
-    scroll: {
-      height: 400,
-    },
+  container: {
+    backgroundColor: '#203038',
+    flex: 1,
+  },
+  primary: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: '15%',
+    marginVertical: '5%',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: 'white'
+  },
+  foodText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: 'white',
+    alignSelf: 'center',
+    marginVertical: '5%'
+  },
+  icon: {
+    marginLeft: '4%'
+  },
+  pieWidget: {
+    backgroundColor: '#3eda9b',
+    borderRadius: 25,
+    alignSelf: 'center',
+    padding: 5
+  },
+  scroll: {
+    height: 400,
+  },
 })
 
