a70e4203d3e73978fe2dfd401f62c20bfd7e9992
Ashley <ashleytyagi@hotmail.com>
2023-03-12 10:04:51 +0000

    Create function to insert calories at user sign up

---
+++ Server/utils/Food/userCaloriesInit.ts
@@ -0,0 +1,25 @@
+require("dotenv").config();
+import { supabaseQueryClass } from "../../utils/databaseInterface";
+import supabase from "../../utils/supabaseSetUp";
+const databaseQuery = new supabaseQueryClass();
+
+export const createCalorieGoal = async (UserID:string,CalorieGoal:number) => {
+    const { data, error }: any = await databaseQuery.insert(
+        supabase,
+        "Calories",
+        {
+            UserID: UserID,
+            Date: new Date().toISOString().split('T')[0],
+            CalorieGoal: CalorieGoal,   
+        }
+    );
+    if (error) {
+        console.log("Error inserting");
+        
+        console.log("Error: ", error);
+        return error;
+    }else{
+        console.log("calorie goal inserted");
+        return data;
+    }
+}
