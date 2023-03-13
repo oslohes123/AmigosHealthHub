9522c0014b0d2b3aa46b3e338eb0219d19bf9491
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2023-03-13 16:09:22 +0000

    Change graph widget to make it reusable. Move everything elese (ie data labels) as parameters

---
+++ Client/navigation/screens/Sleep/hoursSleptGraph.js
@@ -0,0 +1,35 @@
+import { ActivityIndicator, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+import GraphWidget from './graphWidget';
+import moment from 'moment';
+import { useGetSleep } from './hooks/useGetSleep';
+
+export default function hoursSleptGraph() {
+    const { getSleep, isLoading, error } = useGetSleep();
+    const [data, setData] = useState(['test']);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const sleepData = await getSleep();
+            // console.log(
+            //     `Sleep data is in widget: ${JSON.stringify(sleepData)}`
+            // );
+            setData(sleepData);
+        };
+
+        fetchData();
+    }, []);
+
+    const graphTitle = 'Hours slept this week';
+    const label = data.map((item) => moment(item.timestamp).format('ddd'));
+    const dataset = data.map((item) => item.hoursSlept);
+
+    return (
+        <View>
+            {isLoading && <ActivityIndicator />}
+            {error && <Text>{error}</Text>}
+            {!isLoading && !error && GraphWidget(label, dataset, graphTitle)}
+        </View>
+    );
+}
