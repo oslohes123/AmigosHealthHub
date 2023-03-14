2b30d8fc1ed290c61f3e146d96a106d064a8a1a5
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-14 23:22:44 +0000

    create a line chart in stats page

--- Client/navigation/screens/Fitness/screens/Stats.js
+++ Client/navigation/screens/Fitness/screens/Stats.js
@@ -1,10 +1,37 @@
 import { View, StyleSheet, Text, TouchableOpacity, Modal } from 'react-native';
 import React, {useState} from "react";
+import { LineChart } from 'react-native-chart-kit';
+import { Dimensions } from "react-native";
 
 export default function Stats() {
 
     const [selected, setSelected] = useState("")
     const [visible, setVisible] = useState(false)
+
+    const data = {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            data: [20, 45, 28, 80, 99, 43],
+            color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+            strokeWidth: 2 // optional
+          }
+        ],
+        legend: ["Rainy Days"] // optional
+      };
+
+      const screenWidth = Dimensions.get("window").width;
+
+      const chartConfig = {
+        backgroundGradientFrom: "#1E2923",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#08130D",
+        backgroundGradientToOpacity: 0.5,
+        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+      };
 
   return (
     <View style={styles.container}>
@@ -76,6 +103,14 @@ export default function Stats() {
             </Modal>
             </View>
          </View>
+
+         <LineChart
+  data={data}
+  width={screenWidth}
+  height={220}
+  chartConfig={chartConfig}
+/>
+
     </View>
   )
 }
