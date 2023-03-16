3fbfe0b253b44025e95678dbdfbab4dd393b40f2
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-16 12:47:19 +0000

    create widget to view graphs

--- Client/navigation/screens/Fitness/screens/Stats.js
+++ Client/navigation/screens/Fitness/screens/Stats.js
@@ -1,5 +1,7 @@
 import { View, StyleSheet, Text, TouchableOpacity, Modal } from "react-native";
 import React, { useEffect, useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Stats({ navigation }) {
 
@@ -9,13 +11,39 @@ export default function Stats({ navigation }) {
   
   return (
     <View>
-      <TouchableOpacity onPress={pressHandler}>
-        <Text>View Graphs</Text>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={pressHandler}>
+          <LinearGradient
+            colors={["blue", "grey"]}
+            style={styles.widget}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+          >
+            <Text style={styles.header}>
+              Click to view Graphs
+              
+            </Text>
+            <Ionicons name="bar-chart-outline" size={40} color="white" />
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  widget: {
+    paddingHorizontal: "15%",
+    paddingVertical: "10%",
+    borderRadius: 25,
+    width: '80%',
+    alignSelf: 'center',
+    marginTop: '5%',
+    alignItems: 'center'
+  },
+  header: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 25,
+  },
 });
