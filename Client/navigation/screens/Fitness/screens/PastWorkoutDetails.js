289d0fb0fa013d8c560baa542ee7be2c37ff4b24
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-16 23:33:15 +0000

    add bar chart to past workout details screen

--- Client/navigation/screens/Fitness/screens/PastWorkoutDetails.js
+++ Client/navigation/screens/Fitness/screens/PastWorkoutDetails.js
@@ -2,47 +2,21 @@ import React, {useState} from 'react'
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { Calendar } from 'react-native-calendars'
 import { AntDesign } from '@expo/vector-icons';
-import { PieChart } from "react-native-chart-kit";
+import { BarChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
 
 export default function PastWorkoutDetails() {
 
-  const Piedata = [
-    {
-      name: "Protein",
-      amount: 60,
-      color: "orange",
-      legendFontColor: "black",
-      legendFontSize: 18
-    },
-    {
-      name: "Carbs",
-      amount: 120,
-      color: "green",
-      legendFontColor: "black",
-      legendFontSize: 18
-    },
-    {
-      name: "Fat",
-      amount: 25,
-      color: "yellow",
-      legendFontColor: "black",
-      legendFontSize: 18
-    },
-    {
-      name: "Vitamins",
-      amount: 55,
-      color: "blue",
-      legendFontColor: "black",
-      legendFontSize: 18
-    },
-    {
-      name: "Fibre",
-      amount: 34,
-      color: "red",
-      legendFontColor: "black",
-      legendFontSize: 18
-    }
-  ];
+  const screenWidth = Dimensions.get("window").width;
+
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43]
+      }
+    ]
+  };
 
   const [workoutData, setWorkoutData] = useState([]);
 
@@ -72,6 +46,17 @@ export default function PastWorkoutDetails() {
     setViewCalendar(false);
   }
 
+  const chartConfig = {
+    backgroundGradientFrom: "white",
+    //backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#0040ff",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+
   return (
     <View style={styles.container}> 
       <View style={styles.primary}>
@@ -86,17 +71,15 @@ export default function PastWorkoutDetails() {
         </TouchableOpacity>
       </View>
       {selectDay && !viewCalendar  &&  (
-          <TouchableOpacity style={styles.pieWidget}>
-            <PieChart
-              data={Piedata}
-              width={340}
-              height={210}
-              paddingLeft='10'
-              chartConfig={{
-                color: () => "black",
-              }}
-              accessor="amount"
-              backgroundColor="transparent"
+          <TouchableOpacity style={{alignSelf: 'center'}}>
+            <BarChart
+              style={{borderRadius: 25}}
+              data={data}
+              width={0.8 * screenWidth}
+              height={270}
+              yAxisLabel="$"
+              chartConfig={chartConfig}
+              verticalLabelRotation={30}
             />
           </TouchableOpacity>
         )}
@@ -148,12 +131,12 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 40,
     //backgroundColor: 'black'
   },
-  pieWidget: {
-    backgroundColor: '#3eda9b',
-    borderRadius: 25,
-    alignSelf: 'center',
-    padding: 5
-  },
+  // pieWidget: {
+  //   backgroundColor: '#3eda9b',
+  //   borderRadius: 25,
+  //   alignSelf: 'center',
+  //   padding: 5
+  // },
   workoutText: {
     fontSize: 20,
     color: 'white',
