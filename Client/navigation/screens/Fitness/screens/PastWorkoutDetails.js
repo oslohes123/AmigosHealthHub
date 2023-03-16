53d107acef3cb66a0955275d1ec6e98f021426f7
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-16 23:20:13 +0000

    add calendar to past workout details screen

--- Client/navigation/screens/Fitness/screens/PastWorkoutDetails.js
+++ Client/navigation/screens/Fitness/screens/PastWorkoutDetails.js
@@ -1,25 +1,170 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native';
+import React, {useState} from 'react'
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Calendar } from 'react-native-calendars'
+import { AntDesign } from '@expo/vector-icons';
+import { PieChart } from "react-native-chart-kit";
 
 export default function PastWorkoutDetails() {
+
+  const Piedata = [
+    {
+      name: "Protein",
+      amount: 60,
+      color: "orange",
+      legendFontColor: "black",
+      legendFontSize: 18
+    },
+    {
+      name: "Carbs",
+      amount: 120,
+      color: "green",
+      legendFontColor: "black",
+      legendFontSize: 18
+    },
+    {
+      name: "Fat",
+      amount: 25,
+      color: "yellow",
+      legendFontColor: "black",
+      legendFontSize: 18
+    },
+    {
+      name: "Vitamins",
+      amount: 55,
+      color: "blue",
+      legendFontColor: "black",
+      legendFontSize: 18
+    },
+    {
+      name: "Fibre",
+      amount: 34,
+      color: "red",
+      legendFontColor: "black",
+      legendFontSize: 18
+    }
+  ];
+
+  const [workoutData, setWorkoutData] = useState([]);
+
+  const[selectDay, setSelectDay] = useState(null);
+
+  const [viewCalendar, setViewCalendar] = useState(false);
+
+  const currentDate = new Date();
+
+  const markedDate = {
+    [currentDate.toISOString().split('T')[0]]: {
+      selected: true,
+      marked: true,
+      dotColor: 'red'
+    }
+  };
+
+  const[food, setFood] = useState('');
+
+  const toggleCalendar = () => {
+    setViewCalendar(!viewCalendar);
+  }
+
+  const handleDayPress = (day) => {
+    setSelectDay(day.dateString);
+    console.log('selected day', day);
+    setViewCalendar(false);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>PastWorkoutDetails</Text>
-    </View>
+    <View style={styles.container}> 
+      <View style={styles.primary}>
+        {!selectDay && (
+          <Text style={styles.text}>Select a day from the Calendar to View Food History</Text>
+        )}
+        {selectDay && (
+          <Text style={styles.text}>Date: {selectDay}</Text>
+        )}
+        <TouchableOpacity style={styles.icon} onPress={toggleCalendar}>
+          <AntDesign name="calendar" size={35} color="white" />
+        </TouchableOpacity>
+      </View>
+      {selectDay && !viewCalendar  &&  (
+          <TouchableOpacity style={styles.pieWidget}>
+            <PieChart
+              data={Piedata}
+              width={340}
+              height={210}
+              paddingLeft='10'
+              chartConfig={{
+                color: () => "black",
+              }}
+              accessor="amount"
+              backgroundColor="transparent"
+            />
+          </TouchableOpacity>
+        )}
+      {viewCalendar && (
+        <Calendar
+          style={styles.calendar}
+          onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
+          onDayPress={handleDayPress}
+          maxDate={new Date().toISOString().split('T')[0]}
+          markedDates={markedDate}
+        />
+    )}
+     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
       backgroundColor: '#203038',
       flex: 1,
-    },
-    header: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginTop: 30,
-        color: 'white',
-        alignSelf: 'center',
-      },
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 30,
+    color: 'white',
+    alignSelf: 'center',
+  },
+  primary: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: '15%',
+    marginVertical: '5%',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: 'white'
+  },
+  calendar: {
+    width: '90%',
+    alignSelf: 'center',
+    borderTopEndRadius: 40,
+    borderTopLeftRadius: 40,
+    //backgroundColor: 'black'
+  },
+  pieWidget: {
+    backgroundColor: '#3eda9b',
+    borderRadius: 25,
+    alignSelf: 'center',
+    padding: 5
+  },
+  workoutText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    //backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: 'white',
+    alignSelf: 'center',
+    marginVertical: '5%'
+  },
 })
 
