504cdc816d8bf935172cafd6c339baff4ba521df
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2023-03-16 12:44:24 +0000

    Add use add sleep hook to insert sleep data using addSleep api

--- Client/navigation/screens/Sleep/hooks/useAddSleep.js
+++ Client/navigation/screens/Sleep/hooks/useAddSleep.js
@@ -1,27 +1,26 @@
-import moment from 'moment';
 import { useAuthContext } from '../../Authentication/context/AuthContext';
 import { useState } from 'react';
 const port = process.env['PORT'];
 const ip_address = process.env['IP_ADDRESS'];
 
-export const useGetSleep = () => {
+export const useAddSleep = () => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
-    const { user, dispatch } = useAuthContext();
+    const { user } = useAuthContext();
     const id = user.id;
     const token = user.token;
 
-    const getSleep = async () => {
+    const addSleep = async (hoursSleptInput, sleepQualityInput, timestampInput) => {
         setIsLoading(true);
-        const today = moment().format('YYYY-MM-DD');
-        const sevenDaysAgo = moment().subtract(6, 'days').format('YYYY-MM-DD');
 
-        // console.log(`full getSleep user: ${JSON.stringify(user)}`);
-        // console.log(`getUserInfo ip_address: ${ip_address} : Port ${port}`);
-        // console.log(`id in getUserInfo: ${id}`);
-        // console.log(`Time from ${today} to ${sevenDaysAgo}  `);
+        console.log(`full addSleep user: ${JSON.stringify(user)}`);
+        console.log(`getUserInfo ip_address: ${ip_address} : Port ${port}`);
+        console.log(`id in getUserInfo: ${id}`);
+        console.log(
+            `useAddSleep data: hoursslept ${hoursSleptInput}, sleepquality ${sleepQualityInput}, timestamp ${timestampInput}`
+        );
         const response = await fetch(
-            `http://${ip_address}:${port}/api/user/sleep/get`,
+            `http://${ip_address}:${port}/api/user/sleep/add`,
             {
                 method: 'POST',
                 headers: {
@@ -30,14 +29,15 @@ export const useGetSleep = () => {
                 },
                 body: JSON.stringify({
                     userID: id,
-                    startDate: sevenDaysAgo,
-                    endDate: today
+                    hoursSlept: hoursSleptInput,
+                    sleepQaulity: sleepQualityInput,
+                    timestamp: timestampInput
                 })
             }
         );
-        // console.log(`respornse of getSleep: ${JSON.stringify(response)}`);
+        console.log(`respornse of addSleep: ${JSON.stringify(response)}`);
         const responseJSON = await response.json();
-        // console.log(responseJSON);
+        console.log(responseJSON);
         if (!response.ok) {
             setIsLoading(false);
             setError(responseJSON.mssg);
@@ -49,5 +49,5 @@ export const useGetSleep = () => {
         }
     };
 
-    return { getSleep, isLoading, error };
+    return { addSleep, isLoading, error };
 };
