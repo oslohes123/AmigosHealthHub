f5079e4cd52b34adc60f0b4665eed0a06000bb9a
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2023-03-17 10:49:01 +0000

    Add new rgba color for graph's text dark mode

--- Client/navigation/theme/theme.js
+++ Client/navigation/theme/theme.js
@@ -1,16 +1,18 @@
-const theme= {
+const theme = {
     light: {
         theme: 'light',
         color: 'black',
+        colorRGBA: 'rgba(47, 71, 82, 1)',
         background: '#fff',
         secondary: '#e0e0e0'
     },
     dark: {
         theme: 'dark',
         color: 'white',
+        colorRGBA: 'rgba(255, 255, 255, 1)',
         background: '#203038',
         secondary: '#30444e'
     }
-}
+};
 
 export default theme;
