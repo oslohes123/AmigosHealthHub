90f982168812b876a7b589d73705bba4d83415b5
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2023-03-17 10:49:41 +0000

    Add isfocused to refresh the graph whenever screen is rendered

--- Client/navigation/screens/Sleep/hoursSleptGraph.js
+++ Client/navigation/screens/Sleep/hoursSleptGraph.js
@@ -1,25 +1,24 @@
-import { ActivityIndicator, Text, View } from 'react-native';
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
 
 import GraphWidget from '../../components/graphWidget';
 import moment from 'moment';
 import { useGetSleep } from './hooks/useGetSleep';
+import { useIsFocused } from '@react-navigation/native';
 
-export default function hoursSleptGraph() {
+export default function HoursSleptGraph({ navigation }) {
     const { getSleep, isLoading, error } = useGetSleep();
     const [data, setData] = useState(['test']);
+    const isFocused = useIsFocused();
 
     useEffect(() => {
         const fetchData = async () => {
             const sleepData = await getSleep();
-            // console.log(
-            //     `Sleep data is in widget: ${JSON.stringify(sleepData)}`
-            // );
             setData(sleepData);
         };
-
         fetchData();
-    }, []);
+    }, [navigation, isFocused]);
 
     const graphTitle = 'Hours slept this week';
     const label = data.map((item) => moment(item.timestamp).format('ddd'));
@@ -27,10 +26,19 @@ export default function hoursSleptGraph() {
     const graphAttributes = {
         yAxisSuffix: ' h'
     };
-
+    console.log(`label: ${label}`);
+    console.log(`dataset: ${dataset}`);
     return (
         <View>
-            {isLoading && <ActivityIndicator />}
+            {isLoading && (
+                <>
+                    <ActivityIndicator
+                        animating={true}
+                        size={25}
+                        color={MD2Colors.greenA400}
+                    />
+                </>
+            )}
             {error && <Text>{error}</Text>}
             {!isLoading &&
                 !error &&
