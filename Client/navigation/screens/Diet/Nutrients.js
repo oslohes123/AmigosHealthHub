4cca8a8e16d1c22341fe64c06664dcd7f14db7d3
Ashley <ashleytyagi@hotmail.com>
2023-03-17 18:30:03 +0000

    Update nurtition table

--- Client/navigation/screens/Diet/Nutrients.js
+++ Client/navigation/screens/Diet/Nutrients.js
@@ -8,102 +8,33 @@ export default function Nutrients({ route, navigation}) {
   const theme = useContext(themeContext)
   const data = route.params;
 
-  // const dataItems =  data.map((item,index) => {
-  //   return (
-  //     <View key = {index} style={styles.primaryView}>
-  //       <Text style={[styles.text, { color: theme.color }, {borderColor: theme.color}]}>{item.name}</Text>
-  //       <Text style={[styles.text, { color: theme.color }, {borderColor: theme.color}]}>{item.amount}</Text>
-  //     </View>
+  const dataItems =  data.map((item,index) => {
+    return (
+      <DataTable.Row key = {index}>
+        <DataTable.Cell style={[{  color: theme.color }, {borderColor: theme.color}]}>{item.name}</DataTable.Cell>
+        <DataTable.Cell numeric style={[{ color: theme.color }, {borderColor: theme.color}]}>{item.amount}</DataTable.Cell>
+      </DataTable.Row>
+    )
+  })
 
-  //   )
-  // })
 
-
-  // return (
-  //   <View style={[styles.container, { backgroundColor: theme.background }]}>
-  //     <Text style={[styles.header, { color: theme.color }, {borderColor: theme.color}]}>Nutrients Consumed</Text>
-      
-  //     <View style={styles.primaryView}>
-  //       <Text style={[styles.primaryHeader, { color: theme.color }, {borderColor: theme.color}]}>Nutrient</Text>
-  //       <Text style={[styles.primaryHeader, { color: theme.color }, {borderColor: theme.color}]}>Amount</Text>
-  //     </View>
-  //     {dataItems}
-
-  //   </View>
-  // );
   return (
     <View style={[styles.container, { backgroundColor: theme.background }]}>
       <Text style={[styles.header, { color: theme.color }, {borderColor: theme.color}]}>Nutrients Consumed</Text>
       <View style={styles.tableContainer}>
-      <DataTable >
-        <DataTable.Header style={{borderBottomWidth: 5, borderBottomColor: 'black'}}>
-          <DataTable.Title style={{ flex: 1.5}}>Nutrient</DataTable.Title>
-          <DataTable.Title style={{ flex: 1}}>Amount</DataTable.Title>
+        <DataTable>
+        <DataTable.Header>
+          <DataTable.Title >Nutrient</DataTable.Title>
+          <DataTable.Title >Amount</DataTable.Title>
         </DataTable.Header>
-    
-        <DataTable.Row style={styles.row}>
-          <DataTable.Cell style={{ flex: 1.5,}}>item.name</DataTable.Cell>
-          <DataTable.Cell style={{ flex: 1 }}>item.amount</DataTable.Cell>
-        </DataTable.Row>
+        {dataItems}
+        </DataTable>
+      </View>
 
-        <DataTable.Row style={styles.row}>
-          <DataTable.Cell style={{ flex: 1.5 }}>item.name</DataTable.Cell>
-          <DataTable.Cell style={{ flex: 1 }}>item.amount</DataTable.Cell>
-        </DataTable.Row>
-
-        <DataTable.Row style={styles.row}>
-          <DataTable.Cell style={{ flex: 1.5 }}>item.name</DataTable.Cell>
-          <DataTable.Cell style={{ flex: 1 }}>item.amount</DataTable.Cell>
-        </DataTable.Row>
-
-        <DataTable.Row style={styles.row}>
-          <DataTable.Cell style={{ flex: 1.5 }}>item.name</DataTable.Cell>
-          <DataTable.Cell style={{ flex: 1 }}>item.amount</DataTable.Cell>
-        </DataTable.Row>
-
-        <DataTable.Row style={styles.row}>
-          <DataTable.Cell style={{ flex: 1.5 }}>item.name</DataTable.Cell>
-          <DataTable.Cell style={{ flex: 1 }}>item.amount</DataTable.Cell>
-        </DataTable.Row>
-
-      </DataTable>
     </View>
-    </View>
-    
-  )
+  );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   header: {
-//     fontSize: 30,
-//     fontWeight: 'bold',
-//     color: 'white',
-//     alignSelf: 'center',
-//     marginVertical: '5%'
-//   },
-//   primaryHeader: {
-//     fontSize: 25,
-//     fontWeight: 'bold',
-//     borderWidth: 2,
-//     width: '70%',
-//     paddingVertical: '13%',
-//   },
-//   primaryView: {
-//     flexDirection: 'row', 
-//     justifyContent: 'space-between',
-//     marginHorizontal: '20%',
-//     alignSelf: 'center',
-//   },
-//   text: {
-//     fontSize: 20,
-//     borderWidth: 2,
-//     width: '70%',
-//     paddingVertical: '7%',
-//   }
-// });
 
 const styles = StyleSheet.create({
   container: {
