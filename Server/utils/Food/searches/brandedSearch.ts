a3950013c5fa72b5dade510991a7829d16a26d29
Ashley <ashleytyagi@hotmail.com>
2023-03-18 17:28:53 +0000

    ESLinting and converting client side code to js

--- Server/utils/Food/searches/brandedSearch.ts
+++ Server/utils/Food/searches/brandedSearch.ts
@@ -1,9 +1,11 @@
-import brandedSearchInterface from "../../../interfaces/Food/api_interfaces/brandedSearchInterface"
-import {baseUrl} from "../../../constants";
-import authHeaders from './headersObject';
-export default async function brandedSearch(nix_item_id: string): Promise<brandedSearchInterface> {
-    return (await fetch(baseUrl + "search/item?nix_item_id=" + nix_item_id, {
-        method: "GET",
-        headers: authHeaders,
-    })).json();
+/* eslint-disable @typescript-eslint/naming-convention */
+// Ignoring this rule because the API returns the data in this format
+import type brandedSearchInterface from '../../../interfaces/Food/api_interfaces/brandedSearchInterface'
+import { baseUrl } from '../../../constants'
+import authHeaders from './headersObject'
+export default async function brandedSearch (nix_item_id: string): Promise<brandedSearchInterface> {
+  return await (await fetch(baseUrl + 'search/item?nix_item_id=' + nix_item_id, {
+    method: 'GET',
+    headers: authHeaders
+  })).json()
 }
