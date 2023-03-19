78504eaaeb825c06753eca8707884b455ba5465a
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-19 15:07:36 +0000

    eslint refactor, remove comments and align

---
+++ Client/navigation/screens/Settings/SleepSettings.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#203038',
+    flex: 1,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 30,
+    color: 'white',
+    alignSelf: 'center',
+  },
+});
+
+export default function SleepSettings() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Sleep Settings</Text>
+    </View>
+  );
+}
--- Client/navigation/theme/theme.js
+++ Client/navigation/theme/theme.js
@@ -1,16 +1,16 @@
-const theme= {
-    light: {
-        theme: 'light',
-        color: 'black',
-        background: '#fff',
-        secondary: '#e0e0e0'
-    },
-    dark: {
-        theme: 'dark',
-        color: 'white',
-        background: '#203038',
-        secondary: '#30444e'
-    }
-}
+const theme = {
+  light: {
+    theme: 'light',
+    color: 'black',
+    background: '#fff',
+    secondary: '#e0e0e0',
+  },
+  dark: {
+    theme: 'dark',
+    color: 'white',
+    background: '#203038',
+    secondary: '#30444e',
+  },
+};
 
 export default theme;
