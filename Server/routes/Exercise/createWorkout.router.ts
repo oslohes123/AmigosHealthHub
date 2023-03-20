f82127cf18abcae828bec8e0be095d5b6c1067fd
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-03-20 21:05:42 +0000

    Fix ESLint errors in exerciseCalories.controller.ts

--- Server/routes/Exercise/createWorkout.router.ts
+++ Server/routes/Exercise/createWorkout.router.ts
@@ -1,11 +1,11 @@
-const express = require('express');
-const createWorkoutRouter = express.Router();
-createWorkoutRouter.use(express.json());
-import { createWorkout, deleteWorkoutPlan } from "./createWorkout.controller";
-import RouteNamesClass from "../../utils/routeNamesClass";
+import { createWorkout, deleteWorkoutPlan } from './createWorkout.controller'
+import RouteNamesClass from '../../utils/routeNamesClass'
+const express = require('express')
+const createWorkoutRouter = express.Router()
+createWorkoutRouter.use(express.json())
 const routeNames = new RouteNamesClass()
-//Routes
-createWorkoutRouter.post(routeNames.partialAddWorkout, createWorkout);
+// Routes
+createWorkoutRouter.post(routeNames.partialAddWorkout, createWorkout)
 createWorkoutRouter.delete(routeNames.partialDeleteWorkout, deleteWorkoutPlan)
-export default createWorkoutRouter;
+export default createWorkoutRouter
 export {}
