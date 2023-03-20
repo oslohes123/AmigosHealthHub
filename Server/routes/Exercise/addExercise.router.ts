c14f0e1db20d1bb04d2525ca4c8b09e2d69a459d
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-03-20 20:54:52 +0000

    Fix ESLint errors completedWorkouts

--- Server/routes/Exercise/addExercise.router.ts
+++ Server/routes/Exercise/addExercise.router.ts
@@ -1,13 +1,11 @@
-const express = require('express');
-const exerciseRouter = express.Router();
-exerciseRouter.use(express.json());
-// import { addExerciseToExercises, getExerciseByName, searchForExercise } from "./addExercise.controller";
-import { getExerciseByName, searchForExercise } from "./addExercise.controller";
-import RouteNamesClass from "../../utils/routeNamesClass";
+import { getExerciseByName, searchForExercise } from './addExercise.controller'
+import RouteNamesClass from '../../utils/routeNamesClass'
+const express = require('express')
+const exerciseRouter = express.Router()
+exerciseRouter.use(express.json())
 const routeNames = new RouteNamesClass()
-//Routes
-exerciseRouter.get(routeNames.partialSearchExercise, searchForExercise);
-// exerciseRouter.post(routeNames.partialAddToExercises, addExerciseToExercises)
-exerciseRouter.get(routeNames.partialGetExercise, getExerciseByName);
-export default exerciseRouter;
+
+exerciseRouter.get(routeNames.partialSearchExercise, searchForExercise)
+exerciseRouter.get(routeNames.partialGetExercise, getExerciseByName)
+export default exerciseRouter
 export {}
