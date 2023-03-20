774d219c107b8cd2f20d4bb33aa566ed4dfab223
Ashley <ashleytyagi@hotmail.com>
2023-03-20 19:03:52 +0000

    Writing tests for foodSearch

--- Server/test/unit/controllers/Food/foodSearch.test.ts
+++ Server/test/unit/controllers/Food/foodSearch.test.ts
@@ -1,4 +1,81 @@
+import { type Request, type Response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
+import supabase from '../../../../utils/supabaseSetUp'
 import { SupabaseQueryClass } from '../../../../utils/databaseInterface'
-import instantSearch from '../../../../utils/Food/searches/instantFoodSearch'
+import { createHashedPassword, createUser } from '../../../../utils/userFunctions'
+import { clientSearchMethods } from '../../../../constants'
+import { generalSearch } from './../../../../routes/Food/foodSearch.controller'
+import { type ExecutionContext } from 'ava'
 const test = require('ava')
 const sinon = require('sinon')
+const supabaseQuery = new SupabaseQueryClass()
+
+let randomEmail: string
+
+test.before(async (t: any) => {
+  const uuid = uuidv4()
+  randomEmail = `${uuid}@gmail.com`
+
+  const hashedPassword = await createHashedPassword('CorrectPassword123!')
+
+  const { error }: any = await createUser({
+    firstName: 'FoodSearchTest',
+    lastName: 'FoodSearchTest',
+    email: randomEmail,
+    password: hashedPassword,
+    age: 20
+  })
+
+  if (error) {
+    t.fail('Inserting user went wrong!')
+  }
+})
+
+test.after.always(async () => {
+  await supabaseQuery.deleteFrom(supabase, 'User', 'email', randomEmail)
+})
+
+const mockResponse = () => {
+  const res: any = {}
+  res.status = sinon.stub().returns(res)
+  res.json = sinon.stub().returns(res)
+  return res
+}
+
+const mockRequest = (sessionParams: object, sessionData: object) => {
+  return {
+    params: sessionParams,
+    body: sessionData
+  }
+}
+
+test('testing InstantSearch', async (t: ExecutionContext) => {
+  const req = mockRequest({
+    value: 'apple',
+    code: clientSearchMethods.genericSearch
+  }, {})
+  const res = mockResponse()
+  await generalSearch(req as Request, res as Response)
+  t.true(res.status.calledWith(200))
+})
+
+// This test fails as 99 is not a valid code
+test('testing wrong Search code', async (t: ExecutionContext) => {
+  const req = mockRequest({
+    value: 'apple',
+    code: 99
+  }, {})
+  const res = mockResponse()
+  await generalSearch(req as Request, res as Response)
+  t.true(res.status.calledWith(500))
+})
+
+test('Testing specific search', async (t: ExecutionContext) => {
+  const req = mockRequest({
+    value: 'apple',
+    code: clientSearchMethods.specificSearch
+  }, {})
+  const res = mockResponse()
+  await generalSearch(req as Request, res as Response)
+  t.true(res.status.calledWith(200))
+})
