4c9134b57d270c4fa61ff7415b0c5a91b6c1dcf2
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-03-20 22:36:26 +0000

    Change import statments to default

--- Client/navigation/components/CaloriesBurntTodayWidget.js
+++ Client/navigation/components/CaloriesBurntTodayWidget.js
@@ -1,17 +1,19 @@
-import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
-import React, { useEffect, useState } from "react";
-import { useAuthContext } from "../screens/Authentication/context/AuthContext";
-import { useGetCaloriesBurntToday } from "../screens/Fitness/hooks/calories/useGetCaloriesBurntToday";
-import { useIsFocused } from "@react-navigation/native";
-import { LinearGradient } from "expo-linear-gradient";
-import { Animated } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
-import { ActivityIndicator, MD2Colors } from "react-native-paper";
+import {
+  TouchableOpacity, Text, View, StyleSheet,
+  Animated,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
+import useGetCaloriesBurntToday from '../screens/Fitness/hooks/calories/useGetCaloriesBurntToday';
+import { useAuthContext } from '../screens/Authentication/context/AuthContext';
 
 export default function CaloriesBurntTodayWidget() {
-  const { getCaloriesBurntToday, error, isLoading } =
-    useGetCaloriesBurntToday();
+  const { getCaloriesBurntToday, error, isLoading } = useGetCaloriesBurntToday();
   const isFocused = useIsFocused();
   const { user } = useAuthContext();
   const userid = user.id;
@@ -27,25 +29,25 @@ export default function CaloriesBurntTodayWidget() {
     }
   }, [navigation, isFocused]);
   const pressHandler = () => {
-    navigation.navigate("View Stats");
+    navigation.navigate('View Stats');
   };
 
   return (
-    // <View style={styles.container}>
-    //   {error && <Text>{error}</Text>}
+  // <View style={styles.container}>
+  //   {error && <Text>{error}</Text>}
 
-    //   <View style={styles.widget}>
-    //     <Text style={styles.header}>Calories Burnt Today:</Text>
-    //     <Text style={styles.number}>{getCaloriesBurnt}</Text>
-    //   </View>
-    // </View>
+  //   <View style={styles.widget}>
+  //     <Text style={styles.header}>Calories Burnt Today:</Text>
+  //     <Text style={styles.number}>{getCaloriesBurnt}</Text>
+  //   </View>
+  // </View>
 
     <TouchableOpacity onPress={pressHandler}>
       <View style={styles.container}>
         <LinearGradient
           // Button Linear Gradient
           // colors={["#00BFFF", "#0040ff"]}
-          colors={["#00ffc8", "#0040ff"]}
+          colors={['#00ffc8', '#0040ff']}
           style={styles.widget}
           start={{ x: 0, y: 0 }}
           end={{ x: 1, y: 1 }}
@@ -62,7 +64,7 @@ export default function CaloriesBurntTodayWidget() {
             <>
               {/* <Text>Refreshing.....</Text> */}
               <ActivityIndicator
-                animating={true}
+                animating
                 size={25}
                 color={MD2Colors.greenA400}
               />
@@ -76,20 +78,20 @@ export default function CaloriesBurntTodayWidget() {
 
 const styles = StyleSheet.create({
   widget: {
-    paddingHorizontal: "10%",
-    paddingVertical: "10%",
+    paddingHorizontal: '10%',
+    paddingVertical: '10%',
     borderRadius: 25,
   },
   header: {
-    color: "white",
-    fontWeight: "bold",
+    color: 'white',
+    fontWeight: 'bold',
     fontSize: 25,
   },
   number: {
-    color: "white",
+    color: 'white',
     fontSize: 25,
     // fontWeight: "bold",
-    textAlign: "center",
+    textAlign: 'center',
   },
   // container: {
   //   backgroundColor: "darkblue",
