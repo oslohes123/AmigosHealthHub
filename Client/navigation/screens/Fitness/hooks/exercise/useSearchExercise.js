c6e0ff585b4f105b7a2e0a25d98db9f750a1fb33
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-03-20 21:43:45 +0000

    Refactor useSearchExercise hook

--- Client/navigation/screens/Fitness/hooks/exercise/useSearchExercise.js
+++ Client/navigation/screens/Fitness/hooks/exercise/useSearchExercise.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+/* eslint-disable consistent-return */
+import { useState } from 'react';
+
 const port = process.env.PORT;
 const ipAddress = process.env.IP_ADDRESS;
 const searchExerciseRoute = `http://${ipAddress}:${port}/api/user/exercise/search`;
 
-export const useSearchExercise = () => {
+export default function useSearchExercise() {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
   // const { dispatch } = useAuthContext();
@@ -12,35 +14,27 @@ export const useSearchExercise = () => {
     setIsLoading(true);
     setError(null);
 
-    console.log("In searchExercise");
-    // console.log(`Port in searchExercise: ${port}`);
-    // console.log(`ipAddress in searchExercise: ${ipAddress}`);
-
     const response = await fetch(searchExerciseRoute, {
-      method: "GET",
-      headers: { "Content-Type": "application/json", wordToSearch },
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json', wordToSearch },
     });
 
     const searchedExercisesJson = await response.json();
-    console.log(searchedExercisesJson);
     if (!response.ok) {
       setIsLoading(false);
       setError(searchedExercisesJson.mssg);
-      console.log(error);
       return [];
     }
     if (response.ok) {
       try {
-        console.log(searchedExercisesJson);
         setIsLoading(false);
-        return searchedExercisesJson.searchedWords; //this is the array of exercises, look at postman
-      } catch (error) {
-        setError(error);
+        return searchedExercisesJson.searchedWords;
+      } catch (caughtError) {
+        setError(caughtError);
         setIsLoading(false);
-        console.error(error);
       }
     }
   };
 
   return { searchExercise, isLoading, error };
-};
+}
