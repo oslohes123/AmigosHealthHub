5f07bd509b753f4db086596eb27f9c18a8274c59
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-21 12:49:14 +0000

    add widget to mental health dashboard

--- Client/navigation/screens/Mental/start.js
+++ Client/navigation/screens/Mental/start.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Button, TextInput,  Dimensions} from "react-native";
+import { StyleSheet, Text, View , Button, TextInput, Dimensions, TouchableOpacity} from "react-native";
 // npm install , npm install react-native-chart-kit , npx expo install react-native-svg ,  npm install react-native-word-cloud, npm install prop-types
 import React , {useEffect, useState, Component} from 'react';
 const screenWidth = Dimensions.get("window").width;
 const colours = ["#ABDEE6","#CBAACB","#FFFFB5","#FFCCB6","#8FCACA","#FFC8A2","#55CBCD","#FCB9AA","#ECD5E3","#C6DBDA","#FED7C3","#A2E1DB","#97C1A9"]
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App({navigation}) {
   const inputScreenButton = () => {
@@ -15,8 +16,37 @@ export default function App({navigation}) {
   //components for the screen
   return (
     <View style={styles.container}>
-    <Button title="Review Your Day" onPress={inputScreenButton}></Button>      
-    <Button title="Review Your Past" onPress={outputScreenButton}></Button>      
+    {/* <Button title="Review Your Day" onPress={inputScreenButton}></Button>      
+    <Button title="Review Your Past" onPress={outputScreenButton}></Button>       */}
+
+      <TouchableOpacity onPress={inputScreenButton}>
+            <LinearGradient
+              colors={['blue', 'grey']}
+              style={styles.widget}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 1 }}
+            >
+              <Text style={styles.header}>
+              Review Your Day
+
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={outputScreenButton}>
+            <LinearGradient
+              colors={['blue', 'grey']}
+              style={styles.widget}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 1 }}
+            >
+              <Text style={styles.header}>
+              Review Your Past
+
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -27,5 +57,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  widget: {
+    paddingHorizontal: '15%',
+    paddingVertical: '10%',
+    borderRadius: 25,
+    width: '80%',
+    alignSelf: 'center',
+    marginTop: '5%',
+    alignItems: 'center',
+  },
+  header: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
   },
 });
