c363c5b4bae17081d5a98a20fec3e4146f7beab6
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-03-24 09:10:48 +0000

    Comment out mental health and sleep tests

---
+++ Server/test/unit/controllers/Exercise/getCaloriesToday.test.ts
@@ -0,0 +1,14 @@
+// import { type Request, type Response } from 'express'
+// import { changePassword } from '../../../../routes/User/changeProfileDetails.controller'
+// import { v4 as uuidv4 } from 'uuid'
+// import supabase from '../../../../utils/supabaseSetUp'
+// import { SupabaseQueryClass } from '../../../../utils/databaseInterface'
+// import { createHashedPassword, createUser } from '../../../../utils/userFunctions'
+import test from 'ava'
+// const sinon = require('sinon')
+// const bcrypt = require('bcrypt')
+// const supabaseQuery = new SupabaseQueryClass()
+
+test('passing test ', (t: any) => {
+  t.pass()
+})
