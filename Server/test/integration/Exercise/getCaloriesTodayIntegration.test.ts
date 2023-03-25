29ca858ca6fa2ab983db42f3f3311de0ec0325fb
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-03-25 09:44:42 +0000

    Complete integration tests for getCaloriesTodayIntegration

--- Server/test/integration/Exercise/getCaloriesTodayIntegration.test.ts
+++ Server/test/integration/Exercise/getCaloriesTodayIntegration.test.ts
@@ -3,7 +3,8 @@ import { v4 as uuidv4 } from 'uuid'
 import supabase from '../../../utils/supabaseSetUp'
 import { SupabaseQueryClass } from '../../../utils/databaseInterface'
 import { createHashedPassword, createToken, createUserWithID, deleteUserRow } from '../../../utils/userFunctions'
-import { deleteMultipleExercises } from '../../../utils/Exercise/insertAndDeleteMultipleExercises'
+import { deleteMultipleExercises, insertMultipleExercises } from '../../../utils/Exercise/insertAndDeleteMultipleExercises'
+import { addCompletedWorkouts } from '../../../utils/Exercise/createNewTrackedWorkout'
 import RouteNamesClass from '../../../utils/routeNamesClass'
 import test from 'ava'
 import request from 'supertest'
@@ -50,8 +51,66 @@ test.serial(`GET ${getCaloriesTodayRoute} with no userid provided should return 
     .get(getCaloriesTodayRoute)
     .set('authorization', token)
 
-  console.log(`response: ${JSON.stringify(response)}`)
   t.true(response.status === 400)
   t.true(response.headers['content-type'] === 'application/json; charset=utf-8')
   t.true(JSON.stringify(response.body) === JSON.stringify({ mssg: 'Something went wrong!', dev: 'userid does not follow the schema' }))
+})
+
+test.serial(`GET ${getCaloriesTodayRoute} with a user who has no workouts returns 0 calories`, async (t: any) => {
+  const response = await request(app)
+    .get(getCaloriesTodayRoute)
+    .set({ authorization: token, userid: uuid })
+
+  t.true(response.status === 200)
+  t.true(response.headers['content-type'] === 'application/json; charset=utf-8')
+  t.true(JSON.stringify(response.body) === JSON.stringify({ mssg: 'User has no workouts!', totalCaloriesBurnt: 0 }))
+})
+
+test.serial(`GET ${getCaloriesTodayRoute} with a user with a valid workoutplan returns the correct number of calories burnt`, async (t: any) => {
+  const exercises = {
+    exercises: [
+      {
+        name: `Test Curl ${uuid}`,
+        sets: 3,
+        weight: [12, 12, 12],
+        warmUpSet: false,
+        reps: [12, 6, 5],
+        calories: 500,
+        distance: null,
+        duration: null
+      },
+      {
+        name: `Slow Jog ${uuid}`,
+        sets: null,
+        weight: null,
+        warmUpSet: 'false',
+        reps: null,
+        calories: 500,
+        distance: 5000,
+        duration: 23.00
+      }
+    ]
+  }
+  const { errorInsertingMultipleExercises } = await insertMultipleExercises([
+    { type: 'strength', name: `Test Curl ${uuid}`, muscle: 'bicep', difficulty: 'beginner', instructions: 'curl the weight', equipment: 'dumbbell' },
+    { type: 'strength', name: `Slow Jog ${uuid}`, muscle: 'legs', difficulty: 'beginner', instructions: 'jog', equipment: 'none' }])
+
+  if (errorInsertingMultipleExercises) {
+    t.fail(errorInsertingMultipleExercises)
+  }
+  const { errorAddCompletedWorkouts, success } = await addCompletedWorkouts(uuid, 'Test Tracked Workout', exercises)
+  if (errorAddCompletedWorkouts) {
+    t.fail(errorAddCompletedWorkouts)
+  }
+  if (!success) {
+    t.fail('errorsCreatingNewWorkoutPlan')
+  }
+
+  const response = await request(app)
+    .get(getCaloriesTodayRoute)
+    .set({ authorization: token, userid: uuid })
+
+  t.true(response.status === 200)
+  t.true(response.headers['content-type'] === 'application/json; charset=utf-8')
+  t.true(JSON.stringify(response.body) === JSON.stringify({ mssg: 'Success', totalCaloriesBurnt: 1000 }))
 })
