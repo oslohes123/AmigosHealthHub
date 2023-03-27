f1352398d53bcbeaa3514bea31d833424eb2ba9f
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2023-03-27 14:44:51 +0100

    Add supabase functionality to cypress tests

--- Client/cypress/e2e/Sleep/SleepScreen.cy.ts
+++ Client/cypress/e2e/Sleep/SleepScreen.cy.ts
@@ -1,8 +1,49 @@
 /// <reference types="cypress" />
-
+import { v4 as uuidv4 } from 'uuid'
 import supabase from "../../../supabaseSetUp";
 
+const testEmail = `${uuidv4()}@example.com`
+
 before(() => {
-  
-  // cy.visit('http://localhost:19006')
+  async function createUser(){
+    
+    const { error } = await supabase
+        .from('User')
+        .insert({
+          firstName: 'frontend',
+          lastName: 'test',
+          email: testEmail,
+          password: 'TestPassword123!',
+          age: 20
+        })
+        if(error){
+          throw new Error(`Error creating user: ${JSON.stringify(error)}`)
+        }
+  }
+  createUser();
+  cy.visit('http://localhost:19006')
 })
+
+it('should show the login button', () => {
+  cy.contains('Log into account')
+    .should('be.visible')
+})
+  
+// it('should show the sign up button', () => {
+//   cy.contains('Sign up for account')
+//     .should('be.visible')
+// })
+
+after(() => {
+  async function deleteUser(){
+    const { error } = await supabase
+      .from('User')
+      .delete()
+      .eq('email',testEmail)
+      if(error){
+      throw new Error(`Error deleting user: ${JSON.stringify(error)}`)
+      }
+  }
+  console.log(`Deleted user`)
+})
+
