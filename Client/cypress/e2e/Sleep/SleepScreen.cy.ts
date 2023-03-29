d704accd90412829ea69526c65f057196e5e2b6d
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2023-03-29 19:58:20 +0100

    Add sleep tests

--- Client/cypress/e2e/Sleep/SleepScreen.cy.ts
+++ Client/cypress/e2e/Sleep/SleepScreen.cy.ts
@@ -1,67 +1,48 @@
 /// <reference types="cypress" />
 import { v4 as uuidv4 } from 'uuid'
-import supabase from "../../../supabaseSetUp";
 
 const testEmail = `${uuidv4()}@example.com`
 const testPassword = 'TestPassword123!'
 
 before(() => {
-  async function createUser(){
-    
-    const { error } = await supabase
-        .from('User')
-        .insert({
-          firstName: 'frontend',
-          lastName: 'test',
-          email: testEmail,
-          password: testPassword,
-          age: 20
-        })
-        if(error){
-          throw new Error(`Error creating user: ${JSON.stringify(error)}`)
-        }
-  }
-  createUser();
+  cy.visit('http://localhost:19006');
+  cy.contains('Sign up for account')
+    .click()
+  cy.get('input[placeholder="First Name"]').click().type('Test')
+  cy.get('input[placeholder="Last Name"]').click().type('Manik')
+  cy.get('input[placeholder="Email"]').click().type(testEmail)
+  cy.get('input[placeholder="Age"]').click().type('20')
+  cy.get('[data-testid="calorieInput"]').click().type('2500')
+  cy.get('input[placeholder="Password"]').click().type(testPassword)
+  cy.get('input[placeholder="Confirm Password"]').click().type(testPassword)
+  cy.contains('Sign Up').click()
 })
 
 beforeEach(() => {
   cy.visit('http://localhost:19006');
-});
-
-it('should show the login button', () => {
-  cy.contains('Log into account')
-    .should('be.visible')
-})
-  
-it('should show the sign up button', () => {
-  cy.contains('Sign up for account')
-    .should('be.visible')
-})
-
-it('should be able to click on the login button', () => {
-  cy.contains('Log into account')
-    .click()
-    .contains('Log in')
-})
-
-it('should be able to sign in', () => {
   cy.contains('Log into account')
     .click()
   cy.get('input[placeholder="Email"]').click().type(testEmail);
   cy.get('input[placeholder="Password"]').click().type(testPassword);
   cy.contains('Login').click()
+});
+
+it('should be able to sign in', () => {
+
 })
 
 after(() => {
-  async function deleteUser(){
-    const { error } = await supabase
-      .from('User')
-      .delete()
-      .eq('email',testEmail)
-      if(error){
-      throw new Error(`Error deleting user: ${JSON.stringify(error)}`)
-      }
-  }
-  console.log(`Deleted user`)
+  // cy.visit('http://localhost:19006');
+  // cy.contains('Log into account')
+  //   .click()
+  // cy.get('input[placeholder="Email"]').click().type(testEmail);
+  // cy.get('input[placeholder="Password"]').click().type(testPassword);
+  // cy.contains('Login').click()
+  cy.get('a[href="/Settings"]').click()
+  cy.contains(testEmail).click()
+  cy.contains("Delete Account").click()
+  cy.get('input[placeholder="Your password:"]').click().type(testPassword);
+  cy.get('Checkbox[id="confirmDeleteAccount"]').click()
+
 })
 
