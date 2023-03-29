7101eef0b2df042c2b8115a1a97fa6a4e71cc081
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2023-03-29 21:44:44 +0100

    Update sleep screen test

--- Client/cypress/e2e/Sleep/SleepScreen.cy.ts
+++ Client/cypress/e2e/Sleep/SleepScreen.cy.ts
@@ -11,11 +11,15 @@ before(() => {
   cy.get('input[placeholder="First Name"]').click().type('Test')
   cy.get('input[placeholder="Last Name"]').click().type('Manik')
   cy.get('input[placeholder="Email"]').click().type(testEmail)
-  cy.get('input[placeholder="Age"]').click().type('20')
+  cy.get('input[placeholder="Age"]').click().type('50')
   cy.get('[data-testid="calorieInput"]').click().type('2500')
   cy.get('input[placeholder="Password"]').click().type(testPassword)
   cy.get('input[placeholder="Confirm Password"]').click().type(testPassword)
-  cy.contains('Sign Up').click()
+  cy.contains('Sign Up').click().wait(5000)
+  cy.get('a[href="/Settings"]').click()
+  cy.contains('Log out').click()
+  cy.contains('Logout').click()
+
 })
 
 beforeEach(() => {
@@ -27,22 +31,25 @@ beforeEach(() => {
   cy.contains('Login').click()
 });
 
-it('should be able to sign in', () => {
+describe("Check navigate to settings dashboard", () => {
+  it("Should from navigate to settings dashboard" ,() => {
+    cy.get('a[href="/Settings"]').click()
+  })
+})
 
+describe("Check navigate to profile page", () => {
+  it("Should from navigate to profile page" ,() => {
+    cy.get('a[href="/Settings"]').click()
+    cy.contains(testEmail).click()
+  })
 })
 
 after(() => {
-  // cy.visit('http://localhost:19006');
-  // cy.contains('Log into account')
-  //   .click()
-  // cy.get('input[placeholder="Email"]').click().type(testEmail);
-  // cy.get('input[placeholder="Password"]').click().type(testPassword);
-  // cy.contains('Login').click()
   cy.get('a[href="/Settings"]').click()
   cy.contains(testEmail).click()
   cy.contains("Delete Account").click()
-  cy.get('input[placeholder="Your password:"]').click().type(testPassword);
-  cy.get('Checkbox[id="confirmDeleteAccount"]').click()
-
+  cy.get('input[placeholder="Your password:"]').click().type(testPassword, {force: true})
+  cy.get('[data-testid="deletePasswordCheck"]').click()
+  cy.contains("CONFIRM DELETE ACCOUNT").click()
 })
 
