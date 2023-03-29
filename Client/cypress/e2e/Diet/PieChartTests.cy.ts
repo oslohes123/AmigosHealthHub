bf7b7d2181bf7d3765c0355203d46b90b214c2aa
shazeen123 <shazeenshaheen2003@gmail.com>
2023-03-29 22:42:19 +0100

    add tests for diet pie chart

---
+++ Client/cypress/e2e/Diet/PieChartTests.cy.ts
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+beforeEach(() => {
+    cy.visit('http://localhost:19006');
+    cy.contains('Log into account').click()
+    cy.get('input[placeholder="Email"]').click().type('shazeen@gmail.com');
+    cy.get('input[placeholder="Password"]').click().type('Password123!');
+    cy.contains('Login').click()
+    cy.get('a[href="/Diet"]').click();
+})
+
+describe("Check diet pie chart", () => {
+    it("pie chart should be visible on diet dashboard", () => {
+        cy.get('[data-testid="pie"]')
+            .should('be.visible')
+    })
+
+    it("pie chart should be clickable", () => {
+        cy.get('[data-testid="pie"]')
+            .click()
+    })
+
+    it("pie chart should navigate to nutrients page", () => {
+        cy.get('[data-testid="pie"]')
+            .click()
+        cy.contains('Nutrients Consumed')
+    })
+})
