09dc73eba72abb9f49efd4d7bf28ae1da900f1aa
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-03-30 22:32:32 +0100

    Add getExerciseByNameIntegration tests

--- Server/test/unit/controllers/Exercise/getExerciseByName.test.ts
+++ Server/test/unit/controllers/Exercise/getExerciseByName.test.ts
@@ -44,3 +44,15 @@ test('getExerciseByName with correct exercisename results in success', async (t:
   t.true(argsPassed.mssg === 'Exercise Matched!')
   t.true(argsPassed.exerciseInformation.name === 'Dumbbell Bench Press')
 })
+
+test('getExerciseByName with random and incorrect exercisename results in error', async (t: any) => {
+  const invalidRequest = cloneDeep(validRequest)
+  invalidRequest.exercisename = 'asdad1d12wdasdaasdsd'
+  const req = mockRequest(invalidRequest)
+  const res = mockResponse()
+  await getExerciseByName(req as Request, res as Response)
+  const argsPassed = res.json.getCall(0).args[0]
+  t.log(`argsPassed in getExerciseByName3: ${JSON.stringify(argsPassed)}`)
+  t.true(res.status.calledWith(400))
+  t.true(res.json.calledWith({ mssg: 'No exercise of that name was found!' }))
+})
