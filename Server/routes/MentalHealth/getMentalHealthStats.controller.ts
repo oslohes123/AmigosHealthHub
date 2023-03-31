9387a0038543549584fbf08d84f52dcde0abbe9d
V1SHAL421 <70663959+V1SHAL421@users.noreply.github.com>
2023-03-31 14:58:02 +0100

    Add comments to getMentalHealthStats

--- Server/routes/MentalHealth/getMentalHealthStats.controller.ts
+++ Server/routes/MentalHealth/getMentalHealthStats.controller.ts
@@ -8,23 +8,21 @@ import validateJSONSchema from '../../utils/validateJSONSchema'
 import { schemaForRequireduserid } from '../../utils/JSONSchemas/schemaForRequireduserid'
 require('dotenv').config()
 
-export const wordValues = async (req: Request, res: Response) => {
+export const wordValues = async (req: Request, res: Response) => { // returns the last seven words inputted by a certain user
   const { userid } = req.headers
   if (!validateJSONSchema(req.headers, schemaForRequireduserid)) {
     return res.status(400).json({ mssg: 'Something went wrong!', dev: 'userid does not follow the schema' })
   }
-  // const { data, error }: any = await returnLastSeven(supabase, 'Mental Health', 'todays_word', 'created_at', id)
   const { data, error }: any = await returnLastSeven(supabase, 'Mental Health', 'todays_word', 'created_at', userid)
   if (error) {
     return res.status(400).json({ mssg: 'Failed to retrieve last 7 words' })
   }
   else {
-    const frequency = wordFreq(getWords(data))
-    // return res.status(200).json({ mssg: 'MentalHealthOverview', words: Array.from(new Set(getWords(data))), freq: frequency })
+    const frequency = wordFreq(getWords(data)) // stores the frequency of each word
     return res.status(200).json({ mssg: 'MentalHealthOverview', words: [...new Set(getWords(data))], freq: frequency })
   }
 }
-export const dateValues = async (req: Request, res: Response) => {
+export const dateValues = async (req: Request, res: Response) => { // returns the last 7 dates where the user has inputted values
   const { userid } = req.headers
   if (!validateJSONSchema(req.headers, schemaForRequireduserid)) {
     return res.status(400).json({ mssg: 'Something went wrong!', dev: 'userid does not follow the schema' })
@@ -34,11 +32,11 @@ export const dateValues = async (req: Request, res: Response) => {
     return res.status(400).json({ mssg: 'Failed to retrieve last 7 dates' })
   }
   else {
-    return res.status(200).json({ mssg: 'Retrieved dates', dates: getDates(data), success: 'successful' })
+    return res.status(200).json({ mssg: 'Retrieved dates', dates: getDates(data), success: 'successful' }) // getDates() changes data to dates only
   }
 }
 
-export const faceValues = async (req: Request, res: Response) => {
+export const faceValues = async (req: Request, res: Response) => { // returns the last seven ids of the faces inputted by a certain user
   const { userid } = req.headers
   if (!validateJSONSchema(req.headers, schemaForRequireduserid)) {
     return res.status(400).json({ mssg: 'Something went wrong!', dev: 'userid does not follow the schema' })
@@ -48,13 +46,13 @@ export const faceValues = async (req: Request, res: Response) => {
     return res.status(400).json({ mssg: 'Failed to retrieve last 7 faces' })
   }
   else {
-    const avg = average(getFaces(data))
+    const avg = average(getFaces(data)) // stores the average of the last seven ids of the faces
     return res.status(200).json({ mssg: 'Retrieved faces', faces: getFaces(data), average: avg, success: 'successful' })
   }
 }
 
-export const todaysValue = async (req: Request, res: Response) => {
-  const todayDate = getDate(moment().format())
+export const todaysValue = async (req: Request, res: Response) => { // returns today's word
+  const todayDate = getDate(moment().format()) // stores today's date
   const { userid } = req.headers
   if (!validateJSONSchema(req.headers, schemaForRequireduserid)) {
     return res.status(400).json({ mssg: 'Something went wrong!', dev: 'userid does not follow the schema' })
